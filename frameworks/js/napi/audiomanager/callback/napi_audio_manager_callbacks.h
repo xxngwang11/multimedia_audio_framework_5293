xxxppp2025work/multimedia_audio_framework_gitcode

IssueNo: Description: 新增堵麦变焦功能
Signed-off-by: wangxi <wangxi126@huawei.com>

frameworks/js/napi/audiomanager/callback/napi_audio_manager_callbacks.h
@@ -25,8 +25,10 @@ namespace OHOS {
 namespace AudioStandard {
 const std::string DEVICE_CHANGE_CALLBACK_NAME = "deviceChange";
 const std::string MIC_STATE_CHANGE_CALLBACK_NAME = "micStateChange";
+const std::string MICRO_PHONE_BLOCKED_CALLBACK_NAME = "microphoneBlocked";
 
-class NapiAudioManagerCallback : public AudioManagerDeviceChangeCallback {
+class NapiAudioManagerCallback : public AudioManagerDeviceChangeCallback,
+    public AudioManagerMicrophoneBlockedCallback {
 public:
     static bool IsSameCallback(napi_env env, napi_value callback, napi_ref refCallback);
 
@@ -34,6 +36,10 @@ public:
     virtual ~NapiAudioManagerCallback();
     void SaveCallbackReference(const std::string &callbackName, napi_value args);
     void OnDeviceChange(const DeviceChangeAction &deviceChangeAction) override;
+    void onMicrophoneBlocked(const MicPhoneBlockedInfo &micPhoneBlockedinfo) override;
+    void SaveMicrophoneBlockedCallbackReference(napi_value callback);
+    void RemoveMicrophoneBlockedCallbackReference(napi_value env, napi_value callback);
+    void RemoveMicrophoneBlockedCallbackCb();
     int32_t GetAudioManagerDeviceChangeCbListSize();
     void SaveAudioManagerDeviceChangeCbRef(DeviceFlag deviceFlag, napi_value callback);
     void RemoveAudioManagerDeviceChangeCbRef(napi_env env, napi_value callback);
@@ -49,15 +55,19 @@ private:
         std::shared_ptr<AutoRef> callback = nullptr;
         std::string callbackName = "unknown";
         DeviceChangeAction deviceChangeAction;
+        MicPhoneBlockedInfo micPhoneBlockedinfo;
     };
 
     void OnJsCallbackDeviceChange(std::unique_ptr<AudioManagerJsCallback> &jsCb);
+    void OnJsCallbackMicrophoneBlocked(std::unique_ptr<AudioManagerJsCallback> &jsCb);
 
     std::mutex mutex_;
     napi_env env_ = nullptr;
     std::shared_ptr<AutoRef> deviceChangeCallback_ = nullptr;
+    std::shared_ptr<AutoRef> onMicroPhoneBlockedCallback_ = nullptr;
     std::list<std::pair<std::shared_ptr<AutoRef>, DeviceFlag>> audioManagerDeviceChangeCbList_;
     std::list<std::pair<std::shared_ptr<AutoRef>, DeviceFlag>> routingManagerDeviceChangeCbList_;
+    std::list<std::shared_ptr<AutoRef>> MicrophoneBlockedCbList_;
 };
 }  // namespace AudioStandard
 }  // namespace OHOS

frameworks/js/napi/audiomanager/napi_audio_routing_manager.cpp
@@ -860,6 +860,8 @@ napi_value NapiAudioRoutingManager::RegisterCallback(napi_env env, napi_value js
         RegisterPreferredInputDeviceChangeCallback(env, argc, args, cbName, napiRoutingMgr);
     } else if (!cbName.compare(AVAILABLE_DEVICE_CHANGE_CALLBACK_NAME)) {
         RegisterAvaiableDeviceChangeCallback(env, argc, args, cbName, napiRoutingMgr);
+    } else if (!cbName.compare(MICRO_PHONE_BLOCKED_CALLBACK_NAME)) {
+        RegisterMicrophoneBlockedCallback(env, argc, args, cbName, napiRoutingMgr);
     } else {
         AUDIO_ERR_LOG("NapiAudioRoutingManager::No such supported");
         NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
@@ -1021,6 +1023,27 @@ void NapiAudioRoutingManager::RegisterAvaiableDeviceChangeCallback(napi_env env,
     cb->SaveRoutingAvailbleDeviceChangeCbRef(usage, args[PARAM2]);
 }
 
+void NapiAudioRoutingManager::RegisterMicrophoneBlockedCallback(napi_env env, size_t argc, napi_value *args,
+        const std::string &cbName, NapiAudioRoutingManager *napiRoutingMgr)
+{
+    napi_valuetype valueType = napi_undefined;
+    napi_typeof(env, args[PARAM1], &valueType);
+    if (valueType != napi_function) {
+        NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
+            "parameter verification failed: The param of deviceFlag must be enum DeviceFlag");
+    }
+    if (!napiRoutingMgr->microphoneBlockedCallbackNapi_) {
+        napiRoutingMgr->microphoneBlockedCallbackNapi_ = std::make_shared<NapiAudioManagerCallback>(env);
+    }
+    int32_t ret = napiRoutingMgr->audioMngr_->SetMicrophoneBlockedCallback(
+        napiRoutingMgr->microphoneBlockedCallbackNapi_);
+    CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, NapiAudioError::ThrowError(env, ret),
+        "Registering micro phone blocked Callback Failed %{public}d", ret);
+    std::shared_ptr<NapiAudioManagerCallback> cb =
+        std::static_pointer_cast<NapiAudioManagerCallback>(napiRoutingMgr->microphoneBlockedCallbackNapi_);
+    cb->SaveMicrophoneBlockedCallbackReference(args[PARAM1]);
+}
+
 napi_value NapiAudioRoutingManager::On(napi_env env, napi_callback_info info)
 {
     const size_t requireArgc = ARGS_TWO;
@@ -1075,6 +1098,8 @@ napi_value NapiAudioRoutingManager::UnregisterCallback(napi_env env, napi_value 
         UnregisterPreferredInputDeviceChangeCallback(env, callback, napiRoutingMgr);
     } else if (!callbackName.compare(AVAILABLE_DEVICE_CHANGE_CALLBACK_NAME)) {
         UnregisterAvailableDeviceChangeCallback(env, callback, napiRoutingMgr);
+    } else if (!callbackName.compare(MICRO_PHONE_BLOCKED_CALLBACK_NAME)) {
+        UnRegisterMicrophoneBlockedCallback(env, callback, napiRoutingMgr);
     } else {
         AUDIO_ERR_LOG("off no such supported");
         NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
@@ -1174,6 +1199,30 @@ void NapiAudioRoutingManager::UnregisterAvailableDeviceChangeCallback(napi_env e
     }
 }
 
+void NapiAudioRoutingManager::UnRegisterMicrophoneBlockedCallback(napi_env env, napi_value callback,
+    NapiAudioRoutingManager *napiRoutingMgr)
+{   
+    if (napiRoutingMgr->microphoneBlockedCallbackNapi_ != nullptr) {
+        std::shared_ptr<NapiAudioManagerCallback> cb =
+            std::static_pointer_cast<NapiAudioManagerCallback>(
+            napiRoutingMgr->microphoneBlockedCallbackNapi_);
+        if (callback != nullptr) {
+            cb->RemoveMicrophoneBlockedCallbackReference(env, callback);
+        }
+        if (callback == nullptr || cb->GetRoutingMicrophoneBlockedCbListSize() == 0) {
+            int32_t ret = napiRoutingMgr->audioMngr_->UnsetMicrophoneBlockedCallback(DeviceFlag::ALL_L_D_DEVICES_FLAG,
+                napiRoutingMgr->microphoneBlockedCallbackNapi_);
+            CHECK_AND_RETURN_LOG(ret == SUCCESS, "UnRegisterMicrophoneBlockedCallback Failed");
+            napiRoutingMgr->deviceChangeCallbackNapi_.reset();
+            napiRoutingMgr->deviceChangeCallbackNapi_ = nullptr;
+            return;
+        }
+        cb->RemoveMicrophoneBlockedCallbackCb();
+    } else {
+        AUDIO_ERR_LOG("UnRegisterMicrophoneBlockedCallback: UnRegisterMicrophoneBlockedCallback is null");
+    }
+}
+
 napi_value NapiAudioRoutingManager::Off(napi_env env, napi_callback_info info)
 {
     napi_value undefinedResult = nullptr;
@@ -1213,5 +1262,12 @@ napi_value NapiAudioRoutingManager::Off(napi_env env, napi_callback_info info)
 
     return UnregisterCallback(env, jsThis, callbackName, args[PARAM1]);
 }
+
+int32_t NapiAudioManagerCallback::GetRoutingMicrophoneBlockedCbListSize()
+{
+    std::lock_guard<std::mutex> lock(mutex)_;
+    return MicrophoneBlockedCbList_.size();
+}
+
 }  // namespace AudioStandard
 }  // namespace OHOS

frameworks/native/audiopolicy/include/audio_policy_manager.h
@@ -419,6 +419,12 @@ public:
 
     int32_t InjectInterruption(const std::string networkId, InterruptEvent &event);
 
+    int32_t SetMicrophoneBlockedCallback(const std::string clientId,
+        const std::shared_ptr<AudioManagerMicrophoneBlockedCallback> &callback);
+    
+    int32_t UnMicrophoneBlockedCallback(const std::string clientId,
+        const std::shared_ptr<AudioManagerMicrophoneBlockedCallback> &callback);
+
     int32_t LoadSplitModule(const std::string &splitArgs, const std::string &networkId);
 
     int32_t SetDefaultOutputDevice(const DeviceType deviceType, const uint32_t sessionID,

frameworks/native/ohaudio/OHAudioRoutingManager.cpp
@@ -213,6 +213,17 @@ OH_AudioCommon_Result OH_AudioRoutingManager_ReleaseDevices(
     return AUDIOCOMMON_RESULT_SUCCESS;
 }
 
+OH_AudioCommon_Result OH_AudioRoutingManager_SetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager *audioRoutingManager,
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback, void* userData)
+{
+    OHAudioRoutingManager* ohAudioRoutingManager = convertManager(audioRoutingManager);
+    CHECK_AND_RETURN_RET_LOG(ohAudioRoutingManager != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "audioRoutingManager is nullptr");
+    ohAudioRoutingManager->SetMicrophoneBlockedCallback(callback, userData);
+    return AUDIOCOMMON_RESULT_SUCCESS;
+}
+
 namespace OHOS {
 namespace AudioStandard {
 
@@ -401,6 +412,82 @@ void OHAudioDeviceChangedCallback::OnDeviceChange(const DeviceChangeAction &devi
         }
     }
     callback_(type, audioDeviceDescriptorArray);
+}
+
+void OHMicroPhoneBlockCallback::OnMicrophoneBlocked(const MicPhoneBlockedInfo &micPhoneBlockedInfo)
+{
+    CHECK_AND_RETURN_LOG(blockedCallback_ != nullptr, "failed, pointer to the fuction is nullptr");
+    bool isBlocked = micPhoneBlockedInfo.isBlocked_;
+    uint32_t size = micPhoneBlockedInfo.deviceDescriptor.size();
+    if (size <= 0) {
+        AUDIO_ERR_LOG("audioDeviceDescriptors is null");
+        return;
+    }
+    
+    OH_AudioDeviceDescriptorArray *audioDeviceDescriptorArray =
+        (OH_AudioDeviceDescriptorArray *)malloc(sizeof(OH_AudioDeviceDescriptorArray));
+    if (audioDeviceDescriptorArray) {
+        audioDeviceDescriptorArray->descriptors =
+            (OH_AudioDeviceDescriptor**)malloc(sizeof(OH_AudioDeviceDescriptor*) * size);
+        if (audioDeviceDescriptorArray->descriptors == nullptr) {
+            free(audioDeviceDescriptorArray);
+            audioDeviceDescriptorArray = nullptr;
+            AUDIO_ERR_LOG("failed to malloc descriptors.");
+            return;
+        }
+        audioDeviceDescriptorArray->size = size;
+        uint32_t index = 0;
+        for (auto deviceDescriptor : micPhoneBlockedInfo.deviceDescriptors) {
+            audioDeviceDescriptorArray->descriptors[index] =
+                (OH_AudioDeviceDescriptor *)(new OHAudioDeviceDescriptor(deviceDescriptor));
+            if (audioDeviceDescriptorArray->descriptors[index] == nullptr) {
+                DestroyAudioDeviceDescriptor(audioDeviceDescriptorArray);
+                return;
+            }
+            index++;
+        }
+    }
+    blockedCallback_(isBlocked, audioDeviceDescriptorArray，nullptr);
+}
+
+OH_AudioCommon_Result OHAudioRoutingManager::SetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback，void* userData)
+{
+    CHECK_AND_RETURN_RET_LOG(audioSystemManager_ != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "failed, audioSystemManager is null");
+    if (callback == nullptr) {
+        UnSetMicrophoneBlockedCallback(callback);
+        return AUDIOCOMMON_RESULT_SUCCESS;
+    }
+    std::shared_ptr<OHMicroPhoneBlockCallback> microphoneBlock =
+        std::make_shared<OHMicroPhoneBlockCallback>(callback, userData);
+    if (microphoneBlock) {
+        audioSystemManager_->SetMicrophoneBlockedCallback(microphoneBlock);
+        ohMicroPhoneBlockCallbackArray_.push_back(microphoneBlock);
+        return AUDIOCOMMON_RESULT_SUCCESS;
+    }
+    return AUDIOCOMMON_RESULT_ERROR_NO_MEMORY;
+}
+
+OH_AudioCommon_Result OHAudioRoutingManager::UnSetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback)
+{
+    CHECK_AND_RETURN_RET_LOG(audioSystemManager_ != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "failed, audioSystemManager is null");
+
+    audioSystemManager_->UnMicrophoneBlockedCallback();
+
+    auto iter = std::find_if(ohMicroPhoneBlockCallbackArray_.begin(), ohMicroPhoneBlockCallbackArray_.end(),
+        [&](const std::shared_ptr<OHMicroPhoneBlockCallback> &item) {
+        return item->GetCallback() == callback;
+    });
+    if (iter == ohMicroPhoneBlockCallbackArray_.end()) {
+        AUDIO_ERR_LOG("wky iter == ohMicroPhoneBlockCallbackArray_.end");
+        return AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM;
+    }
+
+    ohMicroPhoneBlockCallbackArray_.erase(iter);
+    return AUDIOCOMMON_RESULT_SUCCESS;
 }
 }  // namespace AudioStandard
 }  // namespace OHOS

xxngwang11/multimedia_audio_framework_5293

frameworks/js/napi/audiomanager/callback/napi_audio_manager_callbacks.h
@@ -25,8 +25,10 @@ namespace OHOS {
 namespace AudioStandard {
 const std::string DEVICE_CHANGE_CALLBACK_NAME = "deviceChange";
 const std::string MIC_STATE_CHANGE_CALLBACK_NAME = "micStateChange";
+const std::string MICRO_PHONE_BLOCKED_CALLBACK_NAME = "microphoneBlocked";
 
-class NapiAudioManagerCallback : public AudioManagerDeviceChangeCallback {
+class NapiAudioManagerCallback : public AudioManagerDeviceChangeCallback,
+    public AudioManagerMicrophoneBlockedCallback {
 public:
     static bool IsSameCallback(napi_env env, napi_value callback, napi_ref refCallback);
 
@@ -34,6 +36,11 @@ public:
     virtual ~NapiAudioManagerCallback();
     void SaveCallbackReference(const std::string &callbackName, napi_value args);
     void OnDeviceChange(const DeviceChangeAction &deviceChangeAction) override;
+    void onMicrophoneBlocked(const MicPhoneBlockedInfo &micPhoneBlockedinfo) override;
+    void SaveMicrophoneBlockedCallbackReference(napi_value callback);
+    void RemoveMicrophoneBlockedCallbackReference(napi_env env, napi_value callback);
+    void RemoveMicrophoneBlockedCallbackCb();
+    int32_t GetRoutingMicrophoneBlockedCbListSize();
     int32_t GetAudioManagerDeviceChangeCbListSize();
     void SaveAudioManagerDeviceChangeCbRef(DeviceFlag deviceFlag, napi_value callback);
     void RemoveAudioManagerDeviceChangeCbRef(napi_env env, napi_value callback);
@@ -49,15 +56,19 @@ private:
         std::shared_ptr<AutoRef> callback = nullptr;
         std::string callbackName = "unknown";
         DeviceChangeAction deviceChangeAction;
+        MicPhoneBlockedInfo micPhoneBlockedinfo;
     };
 
     void OnJsCallbackDeviceChange(std::unique_ptr<AudioManagerJsCallback> &jsCb);
+    void OnJsCallbackMicrophoneBlocked(std::unique_ptr<AudioManagerJsCallback> &jsCb);
 
     std::mutex mutex_;
     napi_env env_ = nullptr;
     std::shared_ptr<AutoRef> deviceChangeCallback_ = nullptr;
+    std::shared_ptr<AutoRef> onMicroPhoneBlockedCallback_ = nullptr;
     std::list<std::pair<std::shared_ptr<AutoRef>, DeviceFlag>> audioManagerDeviceChangeCbList_;
     std::list<std::pair<std::shared_ptr<AutoRef>, DeviceFlag>> routingManagerDeviceChangeCbList_;
+    std::list<std::shared_ptr<AutoRef>> MicrophoneBlockedCbList_;
 };
 }  // namespace AudioStandard
 }  // namespace OHOS

frameworks/js/napi/audiomanager/napi_audio_routing_manager.cpp
@@ -860,6 +860,8 @@ napi_value NapiAudioRoutingManager::RegisterCallback(napi_env env, napi_value js
         RegisterPreferredInputDeviceChangeCallback(env, argc, args, cbName, napiRoutingMgr);
     } else if (!cbName.compare(AVAILABLE_DEVICE_CHANGE_CALLBACK_NAME)) {
         RegisterAvaiableDeviceChangeCallback(env, argc, args, cbName, napiRoutingMgr);
+    } else if (!cbName.compare(MICRO_PHONE_BLOCKED_CALLBACK_NAME)) {
+        RegisterMicrophoneBlockedCallback(env, argc, args, cbName, napiRoutingMgr);
     } else {
         AUDIO_ERR_LOG("NapiAudioRoutingManager::No such supported");
         NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
@@ -1021,6 +1023,27 @@ void NapiAudioRoutingManager::RegisterAvaiableDeviceChangeCallback(napi_env env,
     cb->SaveRoutingAvailbleDeviceChangeCbRef(usage, args[PARAM2]);
 }
 
+void NapiAudioRoutingManager::RegisterMicrophoneBlockedCallback(napi_env env, size_t argc, napi_value *args,
+    const std::string &cbName, NapiAudioRoutingManager *napiRoutingMgr)
+{
+    napi_valuetype valueType = napi_undefined;
+    napi_typeof(env, args[PARAM1], &valueType);
+    if (valueType != napi_function) {
+        NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
+            "parameter verification failed: The param of deviceFlag must be enum DeviceFlag");
+    }
+    if (!napiRoutingMgr->microphoneBlockedCallbackNapi_) {
+        napiRoutingMgr->microphoneBlockedCallbackNapi_ = std::make_shared<NapiAudioManagerCallback>(env);
+    }
+    int32_t ret = napiRoutingMgr->audioMngr_->SetMicrophoneBlockedCallback(
+        napiRoutingMgr->microphoneBlockedCallbackNapi_);
+    CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, NapiAudioError::ThrowError(env, ret),
+        "Registering micro phone blocked Callback Failed %{public}d", ret);
+    std::shared_ptr<NapiAudioManagerCallback> cb =
+        std::static_pointer_cast<NapiAudioManagerCallback>(napiRoutingMgr->microphoneBlockedCallbackNapi_);
+    cb->SaveMicrophoneBlockedCallbackReference(args[PARAM1]);
+}
+
 napi_value NapiAudioRoutingManager::On(napi_env env, napi_callback_info info)
 {
     const size_t requireArgc = ARGS_TWO;
@@ -1075,6 +1098,8 @@ napi_value NapiAudioRoutingManager::UnregisterCallback(napi_env env, napi_value 
         UnregisterPreferredInputDeviceChangeCallback(env, callback, napiRoutingMgr);
     } else if (!callbackName.compare(AVAILABLE_DEVICE_CHANGE_CALLBACK_NAME)) {
         UnregisterAvailableDeviceChangeCallback(env, callback, napiRoutingMgr);
+    } else if (!callbackName.compare(MICRO_PHONE_BLOCKED_CALLBACK_NAME)) {
+        UnRegisterMicrophoneBlockedCallback(env, callback, napiRoutingMgr);
     } else {
         AUDIO_ERR_LOG("off no such supported");
         NapiAudioError::ThrowError(env, NAPI_ERR_INVALID_PARAM,
@@ -1174,6 +1199,30 @@ void NapiAudioRoutingManager::UnregisterAvailableDeviceChangeCallback(napi_env e
     }
 }
 
+void NapiAudioRoutingManager::UnRegisterMicrophoneBlockedCallback(napi_env env, napi_value callback,
+    NapiAudioRoutingManager *napiRoutingMgr)
+{   
+    if (napiRoutingMgr->microphoneBlockedCallbackNapi_ != nullptr) {
+        std::shared_ptr<NapiAudioManagerCallback> cb =
+            std::static_pointer_cast<NapiAudioManagerCallback>(
+            napiRoutingMgr->microphoneBlockedCallbackNapi_);
+        if (callback != nullptr) {
+            cb->RemoveMicrophoneBlockedCallbackReference(env, callback);
+        }
+        if (callback == nullptr || cb->GetRoutingMicrophoneBlockedCbListSize() == 0) {
+            int32_t ret = napiRoutingMgr->audioMngr_->UnSetMicrophoneBlockedCallback(
+                napiRoutingMgr->microphoneBlockedCallbackNapi_);
+            CHECK_AND_RETURN_LOG(ret == SUCCESS, "UnSetMicrophoneBlockedCallback Failed");
+            napiRoutingMgr->microphoneBlockedCallbackNapi_.reset();
+            napiRoutingMgr->microphoneBlockedCallbackNapi_ = nullptr;
+            return;
+        }
+        cb->RemoveMicrophoneBlockedCallbackCb();
+    } else {
+        AUDIO_ERR_LOG("UnRegisterMicrophoneBlockedCallback: microphoneBlockedCallbackNapi_ is null");
+    }
+}
+
 napi_value NapiAudioRoutingManager::Off(napi_env env, napi_callback_info info)
 {
     napi_value undefinedResult = nullptr;
@@ -1213,5 +1262,12 @@ napi_value NapiAudioRoutingManager::Off(napi_env env, napi_callback_info info)
 
     return UnregisterCallback(env, jsThis, callbackName, args[PARAM1]);
 }
+
+int32_t NapiAudioManagerCallback::GetRoutingMicrophoneBlockedCbListSize()
+{
+    std::lock_guard<std::mutex> lock(mutex)_;
+    return MicrophoneBlockedCbList_.size();
+}
+
 }  // namespace AudioStandard
 }  // namespace OHOS

frameworks/native/audiopolicy/include/audio_policy_manager.h
@@ -419,6 +419,12 @@ public:
 
     int32_t InjectInterruption(const std::string networkId, InterruptEvent &event);
 
+    int32_t SetMicrophoneBlockedCallback(const std::string clientId,
+        const std::shared_ptr<AudioManagerMicrophoneBlockedCallback> &callback);
+    
+    int32_t UnSetMicrophoneBlockedCallback(const std::string clientId,
+        const std::shared_ptr<AudioManagerMicrophoneBlockedCallback> &callback);
+
     int32_t LoadSplitModule(const std::string &splitArgs, const std::string &networkId);
 
     int32_t SetDefaultOutputDevice(const DeviceType deviceType, const uint32_t sessionID,

frameworks/native/ohaudio/OHAudioRoutingManager.cpp
@@ -213,6 +213,17 @@ OH_AudioCommon_Result OH_AudioRoutingManager_ReleaseDevices(
     return AUDIOCOMMON_RESULT_SUCCESS;
 }
 
+OH_AudioCommon_Result OH_AudioRoutingManager_SetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager *audioRoutingManager,
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback, void* userData)
+{
+    OHAudioRoutingManager* ohAudioRoutingManager = convertManager(audioRoutingManager);
+    CHECK_AND_RETURN_RET_LOG(ohAudioRoutingManager != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "audioRoutingManager is nullptr");
+    ohAudioRoutingManager->SetMicrophoneBlockedCallback(callback, userData);
+    return AUDIOCOMMON_RESULT_SUCCESS;
+}
+
 namespace OHOS {
 namespace AudioStandard {
 
@@ -401,6 +412,81 @@ void OHAudioDeviceChangedCallback::OnDeviceChange(const DeviceChangeAction &devi
         }
     }
     callback_(type, audioDeviceDescriptorArray);
+}
+
+void OHMicroPhoneBlockCallback::OnMicrophoneBlocked(const MicPhoneBlockedInfo &micPhoneBlockedInfo)
+{
+    CHECK_AND_RETURN_LOG(blockedCallback_ != nullptr, "failed, pointer to the fuction is nullptr");
+    bool isBlocked = micPhoneBlockedInfo.isBlocked_;
+    uint32_t size = micPhoneBlockedInfo.deviceDescriptor.size();
+    if (size <= 0) {
+        AUDIO_ERR_LOG("audioDeviceDescriptors is null");
+        return;
+    }
+    
+    OH_AudioDeviceDescriptorArray *audioDeviceDescriptorArray =
+        (OH_AudioDeviceDescriptorArray *)malloc(sizeof(OH_AudioDeviceDescriptorArray));
+    if (audioDeviceDescriptorArray) {
+        audioDeviceDescriptorArray->descriptors =
+            (OH_AudioDeviceDescriptor**)malloc(sizeof(OH_AudioDeviceDescriptor*) * size);
+        if (audioDeviceDescriptorArray->descriptors == nullptr) {
+            free(audioDeviceDescriptorArray);
+            audioDeviceDescriptorArray = nullptr;
+            AUDIO_ERR_LOG("failed to malloc descriptors.");
+            return;
+        }
+        audioDeviceDescriptorArray->size = size;
+        uint32_t index = 0;
+        for (auto deviceDescriptor : micPhoneBlockedInfo.deviceDescriptors) {
+            audioDeviceDescriptorArray->descriptors[index] =
+                (OH_AudioDeviceDescriptor *)(new OHAudioDeviceDescriptor(deviceDescriptor));
+            if (audioDeviceDescriptorArray->descriptors[index] == nullptr) {
+                DestroyAudioDeviceDescriptor(audioDeviceDescriptorArray);
+                return;
+            }
+            index++;
+        }
+    }
+    blockedCallback_(isBlocked, audioDeviceDescriptorArray，nullptr);
+}
+
+OH_AudioCommon_Result OHAudioRoutingManager::SetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback，void* userData)
+{
+    CHECK_AND_RETURN_RET_LOG(audioSystemManager_ != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "failed, audioSystemManager is null");
+    if (callback == nullptr) {
+        UnSetMicrophoneBlockedCallback(callback);
+        return AUDIOCOMMON_RESULT_SUCCESS;
+    }
+    std::shared_ptr<OHMicroPhoneBlockCallback> microphoneBlock =
+        std::make_shared<OHMicroPhoneBlockCallback>(callback, userData);
+    if (microphoneBlock) {
+        audioSystemManager_->SetMicrophoneBlockedCallback(microphoneBlock);
+        ohMicroPhoneBlockCallbackArray_.push_back(microphoneBlock);
+        return AUDIOCOMMON_RESULT_SUCCESS;
+    }
+    return AUDIOCOMMON_RESULT_ERROR_NO_MEMORY;
+}
+
+OH_AudioCommon_Result OHAudioRoutingManager::UnSetMicrophoneBlockedCallback(
+    OH_AudioRoutingManager_OnMicrophoneBlockedCallback callback)
+{
+    CHECK_AND_RETURN_RET_LOG(audioSystemManager_ != nullptr,
+        AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM, "failed, audioSystemManager is null");
+
+    audioSystemManager_->UnSetMicrophoneBlockedCallback();
+
+    auto iter = std::find_if(ohMicroPhoneBlockCallbackArray_.begin(), ohMicroPhoneBlockCallbackArray_.end(),
+        [&](const std::shared_ptr<OHMicroPhoneBlockCallback> &item) {
+        return item->GetCallback() == callback;
+    });
+    if (iter == ohMicroPhoneBlockCallbackArray_.end()) {
+        return AUDIOCOMMON_RESULT_ERROR_INVALID_PARAM;
+    }
+
+    ohMicroPhoneBlockCallbackArray_.erase(iter);
+    return AUDIOCOMMON_RESULT_SUCCESS;
 }
 }  // namespace AudioStandard
 }  // namespace OHOS