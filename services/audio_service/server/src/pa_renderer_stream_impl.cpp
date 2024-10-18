cb54e6caf4b346736af5d356b4835a2c95d3c17c
Wang Chongteng <wangchongteng@huawei.com>
2024-10-18 18:06:02 +0800

    流释放时进行disconnect操作
    Signed-off-by: Wang Chongteng <wangchongteng@huawei.com>

--- services/audio_service/server/src/pa_renderer_stream_impl.cpp
+++ services/audio_service/server/src/pa_renderer_stream_impl.cpp
@@ -370,6 +370,17 @@ int32_t PaRendererStreamImpl::Release()
         std::string sessionIDTemp = std::to_string(streamIndex_);
         audioEffectVolume->StreamVolumeDelete(sessionIDTemp);
     }
+
+    if (paStream_) {
+        pa_stream_set_state_callback(paStream_, nullptr, nullptr);
+        pa_stream_set_write_callback(paStream_, nullptr, nullptr);
+        pa_stream_set_latency_update_callback(paStream_, nullptr, nullptr);
+        pa_stream_set_underflow_callback(paStream_, nullptr, nullptr);
+        pa_stream_set_moved_callback(paStream_, nullptr, nullptr);
+        pa_stream_set_started_callback(paStream_, nullptr, nullptr);
+
+        pa_stream_disconnect(paStream_);
+    }
     
     return SUCCESS;
 }
