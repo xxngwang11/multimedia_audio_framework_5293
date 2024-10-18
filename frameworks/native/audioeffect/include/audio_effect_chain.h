bbcfc7ce5e2abc34876db1390df4821b5a2ede7e
yaojingyu2008 <yaojingyu1@huawei.com>
2024-10-18 16:37:58 +0800

    transfer streamusage
    Signed-off-by: yaojingyu2008 <yaojingyu1@huawei.com>

--- frameworks/native/audioeffect/include/audio_effect_chain.h
+++ frameworks/native/audioeffect/include/audio_effect_chain.h
@@ -92,7 +92,7 @@ private:
     std::string effectMode_ = "";
     uint32_t latency_ = 0;
     uint32_t extraEffectChainType_ = 0;
-    StreamUsage streamUsage_ = STREAM_USAGE_UNKNOWN;
+    StreamUsage streamUsage_ = STREAM_USAGE_INVALID;
     AudioEffectScene currSceneType_ = SCENE_MUSIC;
     std::vector<AudioEffectHandle> standByEffectHandles_;
     std::vector<std::string> effectNames_;
