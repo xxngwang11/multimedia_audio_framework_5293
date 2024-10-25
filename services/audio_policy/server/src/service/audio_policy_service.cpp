ee2e5a3ca32623f0a7374fb117d9491d1075f3a3
Jvgang <yujugang1@h-partners.com>
2024-10-25 20:12:32 +0000

    告警清理：不要混用有符号数和无符号数
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- services/audio_policy/server/src/service/audio_policy_service.cpp
+++ services/audio_policy/server/src/service/audio_policy_service.cpp
@@ -1290,32 +1290,34 @@ void AudioPolicyService::NotifyUserSelectionEventToBt(sptr<AudioDeviceDescriptor
 
 int32_t AudioPolicyService::SetRenderDeviceForUsage(StreamUsage streamUsage, sptr<AudioDeviceDescriptor> desc)
 {
-    auto isPresent = [&desc] (const unique_ptr<AudioDeviceDescriptor> &device) {
+    // get deviceType
+    auto deviceType = [&desc] {
+        switch (streamUsage) {
+            case STREAM_USAGE_VOICE_COMMUNICATION:
+            case STREAM_USAGE_VOICE_MODEM_COMMUNICATION:
+            case STREAM_USAGE_VIDEO_COMMUNICATION:
+                return CALL_OUTPUT_DEVICES;
+            default:
+                return MEDIA_OUTPUT_DEVICES;
+        }
+    }();
+    // find device
+    auto devices = GetAvailableDevicesInner(deviceType);
+    auto itr = std::find_if(devices.begin(), devices.end(), [&desc](const auto device) {
         return (desc->deviceType_ == device->deviceType_) &&
             (desc->macAddress_ == device->macAddress_) &&
             (desc->networkId_ == device->networkId_);
-    };
-    uint32_t tempId = desc->deviceId_;
-    if (streamUsage == STREAM_USAGE_VOICE_COMMUNICATION || streamUsage == STREAM_USAGE_VOICE_MODEM_COMMUNICATION ||
-        streamUsage == STREAM_USAGE_VIDEO_COMMUNICATION) {
-        std::vector<unique_ptr<AudioDeviceDescriptor>> devices = GetAvailableDevicesInner(CALL_OUTPUT_DEVICES);
-        auto itr = std::find_if(devices.begin(), devices.end(), isPresent);
-        CHECK_AND_RETURN_RET_LOG(itr != devices.end(), ERR_INVALID_OPERATION,
-            "device not available type:%{public}d macAddress:%{public}s id:%{public}d networkId:%{public}s",
-            desc->deviceType_, GetEncryptAddr(desc->macAddress_).c_str(),
-            tempId, GetEncryptStr(desc->networkId_).c_str());
-        SetPreferredDevice(AUDIO_CALL_RENDER, new(std::nothrow) AudioDeviceDescriptor(**itr));
-        return SUCCESS;
-    } else {
-        std::vector<unique_ptr<AudioDeviceDescriptor>> devices = GetAvailableDevicesInner(MEDIA_OUTPUT_DEVICES);
-        auto itr = std::find_if(devices.begin(), devices.end(), isPresent);
-        CHECK_AND_RETURN_RET_LOG(itr != devices.end(), ERR_INVALID_OPERATION,
-            "device not available type:%{public}d macAddress:%{public}s id:%{public}d networkId:%{public}s",
-            desc->deviceType_, GetEncryptAddr(desc->macAddress_).c_str(),
-            tempId, GetEncryptStr(desc->networkId_).c_str());
-        SetPreferredDevice(AUDIO_MEDIA_RENDER, new(std::nothrow) AudioDeviceDescriptor(**itr));
-        return SUCCESS;
-    }
+    });
+    auto tempId = desc->deviceId_;
+    CHECK_AND_RETURN_RET_LOG(itr != devices.end(), ERR_INVALID_OPERATION,
+        "device not available type:%{public}d macAddress:%{public}s id:%{public}d networkId:%{public}s",
+        desc->deviceType_, GetEncryptAddr(desc->macAddress_).c_str(),
+        tempId, GetEncryptStr(desc->networkId_).c_str());
+    // set preferred device
+    sptr<AudioDeviceDescriptor> descriptor = new AudioDeviceDescriptor(**itr);
+    CHECK_AND_RETURN_RET_LOG(descriptor != nullptr, ERR_INTERNAL, "Create device descriptor failed");
+    SetPreferredDevice(AUDIO_MEDIA_RENDER, descriptor);
+    return SUCCESS;
 }
 
 int32_t AudioPolicyService::ConnectVirtualDevice(sptr<AudioDeviceDescriptor> &selectedDesc)
@@ -3428,9 +3430,9 @@ int32_t AudioPolicyService::LoadUsbModule(string deviceInfo, DeviceRole deviceRo
         if (configRole != deviceRole) {continue;}
         GetUsbModuleInfo(deviceInfo, moduleInfo);
         if (isEcFeatureEnable_) {
-            uint32_t bufferSize = (std::stoi(moduleInfo.rate) *
+            uint32_t bufferSize = (static_cast<uint32_t>(std::stoi(moduleInfo.rate)) *
                 GetSampleFormatValue(formatStrToEnum[moduleInfo.format]) *
-                std::stoi(moduleInfo.channels)) / PCM_8_BIT * RENDER_FRAME_INTERVAL_IN_SECONDS;
+                static_cast<uint32_t>(std::stoi(moduleInfo.channels))) / PCM_8_BIT * RENDER_FRAME_INTERVAL_IN_SECONDS;
             moduleInfo.bufferSize = std::to_string(bufferSize);
             AUDIO_INFO_LOG("update arm usb buffer size: %{public}s", moduleInfo.bufferSize.c_str());
             if (deviceRole == OUTPUT_DEVICE) {
