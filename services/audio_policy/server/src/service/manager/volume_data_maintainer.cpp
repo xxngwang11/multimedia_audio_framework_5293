467ca4461432cc5d8ade711885c0e03827775c7a
whuqincheng <liweiyang1@huawei.com>
2024-11-18 19:43:50 +0800

    Resolve the issue of incorrect USB volume value after device reboot
    Signed-off-by: whuqincheng <liweiyang1@huawei.com>

--- services/audio_policy/server/src/service/manager/volume_data_maintainer.cpp
+++ services/audio_policy/server/src/service/manager/volume_data_maintainer.cpp
@@ -175,8 +175,8 @@ bool VolumeDataMaintainer::GetVolumeInternal(DeviceType deviceType, AudioStreamT
         return false;
     } else {
         volumeLevelMap_[streamType] = volumeValue;
-        AUDIO_PRERELEASE_LOGI("Get streamType %{public}d, deviceType {public}d, Volume FromDataBase volumeMap from datashare %{public}d.",
-            streamType, deviceType, volumeValue);
+        AUDIO_PRERELEASE_LOGI("Get streamType %{public}d, deviceType {public}d,
+            Volume FromDataBase volumeMap from datashare %{public}d.", streamType, deviceType, volumeValue);
     }
 
     return true;
