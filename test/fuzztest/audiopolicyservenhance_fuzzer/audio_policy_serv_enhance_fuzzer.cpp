9110f022d2d32177a2871dcbc3f236755c6d9862
zhangwt3652 <zhangwenting15@huawei.com>
2024-12-03 12:24:27 +0800

    音频Fuzz整改
    Signed-off-by: 王彬彬 <wangbinbin57@huawei.com>

--- test/fuzztest/audiopolicyservenhance_fuzzer/audio_policy_serv_enhance_fuzzer.cpp
+++ test/fuzztest/audiopolicyservenhance_fuzzer/audio_policy_serv_enhance_fuzzer.cpp
@@ -25,62 +25,33 @@ using namespace std;
 namespace OHOS {
 namespace AudioStandard {
 using namespace std;
+const int32_t LIMITSIZE = 4;
 const uint64_t CAPSESSION_ID = 123456;
-static const uint8_t *RAW_DATA = nullptr;
-static size_t g_dataSize = 0;
-static size_t g_pos;
-const size_t THRESHOLD = 10;
 
-/*
-* describe: get data from outside untrusted data(RAW_DATA) which size is according to sizeof(T)
-* tips: only support basic type
-*/
-template<class T>
-T GetData()
+void AudioEffectServiceFuzzTest(const uint8_t *rawData, size_t size)
 {
-    T object {};
-    size_t objectSize = sizeof(object);
-    if (RAW_DATA == nullptr || objectSize > g_dataSize - g_pos) {
-        return object;
+    if (rawData == nullptr || size < LIMITSIZE) {
+        return;
     }
-    errno_t ret = memcpy_s(&object, objectSize, RAW_DATA + g_pos, objectSize);
-    if (ret != EOK) {
-        return {};
-    }
-    g_pos += objectSize;
-    return object;
-}
 
-template<class T>
-uint32_t GetArrLength(T& arr)
-{
-    if (arr == nullptr) {
-        AUDIO_INFO_LOG("%{public}s: The array length is equal to 0", __func__);
-        return 0;
-    }
-    return sizeof(arr) / sizeof(arr[0]);
-}
-
-void AudioEffectServiceFuzzTest()
-{
     std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler =
         DelayedSingleton<AudioPolicyServerHandler>::GetInstance();
     std::shared_ptr<IAudioInterruptEventDispatcher> dispatcher = nullptr;
     audioPolicyServerHandler->Init(dispatcher);
 
-    int32_t clientPid = GetData<int32_t>();
+    int32_t clientPid = *reinterpret_cast<const int32_t*>(rawData);
     sptr<IAudioPolicyClient> cb = nullptr;
     audioPolicyServerHandler->AddAudioPolicyClientProxyMap(clientPid, cb);
 
-    pid_t removeClientPid = GetData<pid_t>();
+    pid_t removeClientPid = *reinterpret_cast<const pid_t*>(rawData);
     audioPolicyServerHandler->RemoveAudioPolicyClientProxyMap(removeClientPid);
 
-    int32_t clientId = GetData<int32_t>();
+    int32_t clientId = *reinterpret_cast<const int32_t*>(rawData);
     std::shared_ptr<AudioInterruptCallback> audioInterruptCallback = nullptr;
     audioPolicyServerHandler->AddExternInterruptCbsMap(clientId, audioInterruptCallback);
     audioPolicyServerHandler->RemoveExternInterruptCbsMap(clientId);
 
-    AudioDeviceUsage usage = GetData<AudioDeviceUsage>();
+    AudioDeviceUsage usage = D_ALL_DEVICES;
     sptr<IStandardAudioPolicyManagerListener> audioPolicyManagerListener = nullptr;
     audioPolicyServerHandler->AddAvailableDeviceChangeMap(clientId, usage, audioPolicyManagerListener);
     audioPolicyServerHandler->RemoveAvailableDeviceChangeMap(clientId, usage);
@@ -90,15 +61,19 @@ void AudioEffectServiceFuzzTest()
     audioPolicyServerHandler->RemoveDistributedRoutingRoleChangeCbsMap(clientId);
 }
 
-void AudioSendCallbackFuzzTest()
+void AudioSendCallbackFuzzTest(const uint8_t *rawData, size_t size)
 {
+    if (rawData == nullptr || size < LIMITSIZE) {
+        return;
+    }
+
     std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler =
         DelayedSingleton<AudioPolicyServerHandler>::GetInstance();
     std::vector<std::shared_ptr<AudioDeviceDescriptor>> desc;
     audioPolicyServerHandler->SendDeviceChangedCallback(desc, true);
     audioPolicyServerHandler->SendDeviceChangedCallback(desc, false);
 
-    DeviceBlockStatus status = GetData<DeviceBlockStatus>();
+    DeviceBlockStatus status = *reinterpret_cast<const DeviceBlockStatus*>(rawData);
     audioPolicyServerHandler->SendMicrophoneBlockedCallback(desc, status);
 
     audioPolicyServerHandler->SendAvailableDeviceChange(desc, true);
@@ -110,28 +85,32 @@ void AudioSendCallbackFuzzTest()
     std::pair<int32_t, AudioSessionDeactiveEvent> sessionDeactivePair;
     audioPolicyServerHandler->SendAudioSessionDeactiveCallback(sessionDeactivePair);
 
-    int32_t callbackCategory = GetData<int32_t>();
+    int32_t callbackCategory = *reinterpret_cast<const int32_t*>(rawData);
     AudioInterrupt audioInterrupt;
     std::list<std::pair<AudioInterrupt, AudioFocuState>> focusInfoList;
     audioPolicyServerHandler->SendAudioFocusInfoChangeCallback(callbackCategory, audioInterrupt, focusInfoList);
 
-    AudioRingerMode ringMode = GetData<AudioRingerMode>();
+    AudioRingerMode ringMode = RINGER_MODE_NORMAL;
     audioPolicyServerHandler->SendRingerModeUpdatedCallback(ringMode);
 
     MicStateChangeEvent micStateChangeEvent;
-    int32_t clientId = GetData<int32_t>();
+    int32_t clientId = *reinterpret_cast<const int32_t*>(rawData);
     audioPolicyServerHandler->SendMicStateUpdatedCallback(micStateChangeEvent);
     audioPolicyServerHandler->SendMicStateWithClientIdCallback(micStateChangeEvent, clientId);
 
     InterruptEventInternal interruptEvent;
-    uint32_t sessionId = GetData<uint32_t>();
+    uint32_t sessionId = *reinterpret_cast<const uint32_t*>(rawData);
     audioPolicyServerHandler->SendInterruptEventInternalCallback(interruptEvent);
     audioPolicyServerHandler->SendInterruptEventWithSessionIdCallback(interruptEvent, sessionId);
     audioPolicyServerHandler->SendInterruptEventWithClientIdCallback(interruptEvent, clientId);
 }
 
-void AudioPolicyServSendFuzzTest()
+void AudioPolicyServSendFuzzTest(const uint8_t *rawData, size_t size)
 {
+    if (rawData == nullptr || size < LIMITSIZE) {
+        return;
+    }
+
     std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler =
         DelayedSingleton<AudioPolicyServerHandler>::GetInstance();
 
@@ -139,7 +118,7 @@ void AudioPolicyServSendFuzzTest()
     audioPolicyServerHandler->SendPreferredInputDeviceUpdated();
 
     std::shared_ptr<AudioDeviceDescriptor> descriptor;
-    CastType type = GetData<CastType>();
+    CastType type = CAST_TYPE_ALL;
     audioPolicyServerHandler->SendDistributedRoutingRoleChange(descriptor, type);
 
     std::vector<std::shared_ptr<AudioRendererChangeInfo>> audioRendererChangeInfos;
@@ -148,9 +127,9 @@ void AudioPolicyServSendFuzzTest()
     std::vector<std::shared_ptr<AudioCapturerChangeInfo>> audioCapturerChangeInfos;
     audioPolicyServerHandler->SendCapturerInfoEvent(audioCapturerChangeInfos);
 
-    int32_t clientPid = GetData<int32_t>();
-    uint32_t sessionId = GetData<uint32_t>();
-    int32_t streamFlag = GetData<int32_t>();
+    int32_t clientPid = *reinterpret_cast<const int32_t*>(rawData);
+    uint32_t sessionId = *reinterpret_cast<const uint32_t*>(rawData);
+    int32_t streamFlag = *reinterpret_cast<const int32_t*>(rawData);
     AudioDeviceDescriptor outputDeviceInfo(AudioDeviceDescriptor::DEVICE_INFO);
     AudioStreamDeviceChangeReasonExt reason = AudioStreamDeviceChangeReasonExt::ExtEnum::UNKNOWN;
     audioPolicyServerHandler->SendRendererDeviceChangeEvent(clientPid, sessionId, outputDeviceInfo, reason);
@@ -161,7 +140,7 @@ void AudioPolicyServSendFuzzTest()
     AudioCapturerInfo capturerInfo;
     AudioStreamInfo streamInfo;
     uint64_t sendCapturerSessionId = CAPSESSION_ID;
-    int32_t error = GetData<int32_t>();
+    int32_t error = *reinterpret_cast<const int32_t*>(rawData);
     audioPolicyServerHandler->SendCapturerCreateEvent(capturerInfo, streamInfo, sendCapturerSessionId, true, error);
     audioPolicyServerHandler->SendCapturerCreateEvent(capturerInfo, streamInfo, sendCapturerSessionId, false, error);
     audioPolicyServerHandler->SendCapturerRemovedEvent(sendCapturerSessionId, true);
@@ -182,60 +161,34 @@ void AudioPolicyServSendFuzzTest()
     audioPolicyServerHandler->SendHeadTrackingEnabledChangeForAnyDeviceEvent(selectedAudioDevice, true);
     audioPolicyServerHandler->SendHeadTrackingEnabledChangeForAnyDeviceEvent(selectedAudioDevice, false);
 
-    AudioPipeType pipeType = GetData<AudioPipeType>();
+    AudioPipeType pipeType = PIPE_TYPE_MULTICHANNEL;
     audioPolicyServerHandler->SendPipeStreamCleanEvent(pipeType);
 }
 
-void AudioPolicyServHandleFuzzTest()
+void AudioPolicyServHandleFuzzTest(const uint8_t *rawData, size_t size)
 {
+    if (rawData == nullptr || size < LIMITSIZE) {
+        return;
+    }
+
     std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler =
         DelayedSingleton<AudioPolicyServerHandler>::GetInstance();
 
-    CallbackChange callbackchange = GetData<CallbackChange>();
+    CallbackChange callbackchange = CALLBACK_FOCUS_INFO_CHANGE;
     audioPolicyServerHandler->SetClientCallbacksEnable(callbackchange, true);
     audioPolicyServerHandler->SetClientCallbacksEnable(callbackchange, false);
 }
 
-typedef void (*TestFuncs[4])();
-
-TestFuncs g_testFuncs = {
-    AudioEffectServiceFuzzTest,
-    AudioSendCallbackFuzzTest,
-    AudioPolicyServSendFuzzTest,
-    AudioPolicyServHandleFuzzTest,
-};
-
-bool FuzzTest(const uint8_t* rawData, size_t size)
-{
-    if (rawData == nullptr) {
-        return false;
-    }
-
-    // initialize data
-    RAW_DATA = rawData;
-    g_dataSize = size;
-    g_pos = 0;
-
-    uint32_t code = GetData<uint32_t>();
-    uint32_t len = GetArrLength(g_testFuncs);
-    if (len > 0) {
-        g_testFuncs[code % len]();
-    } else {
-        AUDIO_INFO_LOG("%{public}s: The len length is equal to 0", __func__);
-    }
-
-    return true;
-}
 } // namespace AudioStandard
 } // namesapce OHOS
 
 /* Fuzzer entry point */
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *rawData, size_t size)
 {
-    if (size < OHOS::AudioStandard::THRESHOLD) {
-        return 0;
-    }
-
-    OHOS::AudioStandard::FuzzTest(data, size);
+    /* Run your code on data */
+    OHOS::AudioStandard::AudioEffectServiceFuzzTest(rawData, size);
+    OHOS::AudioStandard::AudioSendCallbackFuzzTest(rawData, size);
+    OHOS::AudioStandard::AudioPolicyServSendFuzzTest(rawData, size);
+    OHOS::AudioStandard::AudioPolicyServHandleFuzzTest(rawData, size);
     return 0;
 }
