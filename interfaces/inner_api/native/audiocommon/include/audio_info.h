9a298a5ed242153fc21d0d8d01c2aa5574a1ae68
Jvgang <yujugang1@h-partners.com>
2024-12-17 10:05:20 +0000

    SupportedTones 校验上限值过小，导致拨号盘无声问题修复！
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- interfaces/inner_api/native/audiocommon/include/audio_info.h
+++ interfaces/inner_api/native/audiocommon/include/audio_info.h
@@ -104,7 +104,7 @@ const char* NORMAL_STREAM = "NormalStream";
 // Maximun number of sine waves in a tone segment
 constexpr uint32_t TONEINFO_MAX_WAVES = 3;
 //Maximun number of SupportedTones
-constexpr uint32_t MAX_SUPPORTED_TONEINFO_SIZE= 65535;
+constexpr uint32_t MAX_SUPPORTED_TONEINFO_SIZE = 65535;
 // Maximun number of segments in a tone descriptor
 constexpr uint32_t TONEINFO_MAX_SEGMENTS = 12;
 constexpr uint32_t TONEINFO_INF = 0xFFFFFFFF;
