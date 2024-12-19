xxxppp2025work/multimedia_audio_framework_gitcode

fix fastaudiostream slienmodewitho
Signed-off-by: kw <wangyuqi30@huawei.com>

services/audio_service/common/include/i_audio_process_stream.h
@@ -52,6 +52,10 @@ public:
 
     virtual int32_t SetDefaultOutputDevice(const DeviceType defaultOuputDevice) = 0;
 
+    virtual int32_t SetSilentModeAndMixWithOthers(bool on) = 0;
+
+    virtual bool IsSilentModeAndMixWithOthers() = 0;
+
     virtual ~IAudioProcessStream() = default;
 };
 } // namespace AudioStandard

services/audio_service/server/include/audio_process_in_server.h
@@ -100,6 +100,10 @@ public:
 
     int32_t SetDefaultOutputDevice(const DeviceType defaultOuputDevice) override;
 
+    int32_t SetSilentModeAndMixWithOthers(bool on) override;
+
+    bool IsSilentModeAndMixWithOthers() override;
+
 public:
     const AudioProcessConfig processConfig_;
 
@@ -110,6 +114,7 @@ private:
 
 private:
     std::atomic<bool> muteFlag_ = false;
+    std::atomic<bool> silentModeAndMixWithOthers_ = false;
     bool isInnerCapped_ = false;
     ProcessReleaseCallback *releaseCallback_ = nullptr;
 

services/audio_service/server/src/audio_endpoint.cpp
@@ -1534,6 +1534,7 @@ void AudioEndpointInner::GetAllReadyProcessData(std::vector<AudioStreamData> &au
         AudioVolumeType volumeType = VolumeUtils::GetVolumeTypeFromStreamType(streamType);
         DeviceType deviceType = PolicyHandler::GetInstance().GetActiveOutPutDevice();
         bool muteFlag = processList_[i]->GetMuteFlag();
+        bool isSilentModeAndMixWithOthers = processList_[i]->IsSilentModeAndMixWithOthers();
         if (deviceInfo_.networkId_ == LOCAL_NETWORK_ID &&
             !(deviceInfo_.deviceType_ == DEVICE_TYPE_BLUETOOTH_A2DP && volumeType == STREAM_MUSIC &&
                 PolicyHandler::GetInstance().IsAbsVolumeSupported()) &&
@@ -1550,7 +1551,7 @@ void AudioEndpointInner::GetAllReadyProcessData(std::vector<AudioStreamData> &au
         SpanStatus targetStatus = SpanStatus::SPAN_WRITE_DONE;
         if (curReadSpan->spanStatus.compare_exchange_strong(targetStatus, SpanStatus::SPAN_READING)) {
             processBufferList_[i]->GetReadbuffer(curRead, streamData.bufferDesc); // check return?
-            if (muteFlag) {
+            if (muteFlag || isSilentModeAndMixWithOthers) {
                 memset_s(static_cast<void *>(streamData.bufferDesc.buffer), streamData.bufferDesc.bufLength,
                     0, streamData.bufferDesc.bufLength);
             }

xxngwang11/multimedia_audio_framework_5293

services/audio_service/common/include/i_audio_process_stream.h
@@ -44,13 +44,15 @@ public:
 
     virtual BufferDesc &GetConvertedBuffer() = 0;
 
-    virtual bool GetMuteFlag() = 0;
+    virtual bool GetMuteState() = 0;
 
     virtual AudioProcessConfig GetAudioProcessConfig() = 0;
 
     virtual void WriteDumpFile(void *buffer, size_t bufferSize) = 0;
 
     virtual int32_t SetDefaultOutputDevice(const DeviceType defaultOuputDevice) = 0;
+
+    virtual int32_t SetSilentModeAndMixWithOthers(bool on) = 0;
 
     virtual ~IAudioProcessStream() = default;
 };

services/audio_service/server/include/audio_process_in_server.h
@@ -85,7 +85,7 @@ public:
     int32_t RemoveProcessStatusListener(std::shared_ptr<IProcessStatusListener> listener);
 
     void SetNonInterruptMute(const bool muteFlag);
-    bool GetMuteFlag() override;
+    bool GetMuteState() override;
     uint32_t GetSessionId();
 
     // for inner-cap
@@ -100,6 +100,8 @@ public:
 
     int32_t SetDefaultOutputDevice(const DeviceType defaultOuputDevice) override;
 
+    int32_t SetSilentModeAndMixWithOthers(bool on) override;
+
 public:
     const AudioProcessConfig processConfig_;
 
@@ -110,6 +112,7 @@ private:
 
 private:
     std::atomic<bool> muteFlag_ = false;
+    std::atomic<bool> silentModeAndMixWithOthers_ = false;
     bool isInnerCapped_ = false;
     ProcessReleaseCallback *releaseCallback_ = nullptr;
 

services/audio_service/server/src/audio_endpoint.cpp
@@ -1533,7 +1533,7 @@ void AudioEndpointInner::GetAllReadyProcessData(std::vector<AudioStreamData> &au
         AudioStreamType streamType = processList_[i]->GetAudioStreamType();
         AudioVolumeType volumeType = VolumeUtils::GetVolumeTypeFromStreamType(streamType);
         DeviceType deviceType = PolicyHandler::GetInstance().GetActiveOutPutDevice();
-        bool muteFlag = processList_[i]->GetMuteFlag();
+        bool muteFlag = processList_[i]->GetMuteState();
         if (deviceInfo_.networkId_ == LOCAL_NETWORK_ID &&
             !(deviceInfo_.deviceType_ == DEVICE_TYPE_BLUETOOTH_A2DP && volumeType == STREAM_MUSIC &&
                 PolicyHandler::GetInstance().IsAbsVolumeSupported()) &&
@@ -1984,7 +1984,7 @@ void AudioEndpointInner::WriteToProcessBuffers(const BufferDesc &readBuf)
         }
 
         int32_t ret = WriteToSpecialProcBuf(processBufferList_[i], readBuf, processList_[i]->GetConvertedBuffer(),
-            processList_[i]->GetMuteFlag());
+            processList_[i]->GetMuteState());
         CHECK_AND_CONTINUE_LOG(ret == SUCCESS,
             "endpoint write to process buffer %{public}zu fail, ret %{public}d.", i, ret);
         AUDIO_DEBUG_LOG("endpoint process buffer %{public}zu write success.", i);