4c3d4ccda0ef9bf2bdbbe92fe205f889a76edb22
gonglin123 <gonglin17@huawei.com>
2024-12-21 13:40:25 +0000

    修改codecheck
    Signed-off-by: gonglin123 <gonglin17@huawei.com>

--- services/audio_policy/test/unittest/audio_pnp_server_unit_test/src/audio_pnp_server_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_pnp_server_unit_test/src/audio_pnp_server_unit_test.cpp
@@ -1,7 +1,4 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2020-2020. All rights reserved.
- */
-/*
  * Copyright (c) 2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
