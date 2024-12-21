4aa51c532c0f55f5cdb1289166fad16038d4d9e4
Jvgang <yujugang1@h-partners.com>
2024-12-21 12:25:42 +0000

    Allow using fd:0 when creating AudioBuffer and add logging when closing fd
    Signed-off-by: Jvgang <yujugang1@h-partners.com>

--- frameworks/native/audioutils/src/audio_utils.cpp
+++ frameworks/native/audioutils/src/audio_utils.cpp
@@ -830,8 +830,9 @@ void CloseFd(int fd)
     if (fd == STDIN_FILENO || fd == STDOUT_FILENO || fd == STDERR_FILENO) {
         AUDIO_WARNING_LOG("special fd: %{public}d will be closed", fd);
     }
+    int tmpFd = fd;
     close(fd);
-    AUDIO_DEBUG_LOG("fd: %{public}% closed successfuly!", fd);
+    AUDIO_DEBUG_LOG("fd: %{public}% closed successfuly!", tmpFd);
 }
 
 static void MemcpyToI32FromI16(int16_t *src, int32_t *dst, size_t count)
