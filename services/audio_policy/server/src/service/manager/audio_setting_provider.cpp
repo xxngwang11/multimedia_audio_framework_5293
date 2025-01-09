684b97f4c71c3579444e20f9016b5f68da256d82
Chumouzhi <chumouzhi@h-partners.com>
2025-01-09 06:57:18 +0000

    update services/audio_policy/server/src/service/manager/audio_setting_provider.cpp.
    Signed-off-by: Chumouzhi <chumouzhi@h-partners.com>

--- services/audio_policy/server/src/service/manager/audio_setting_provider.cpp
+++ services/audio_policy/server/src/service/manager/audio_setting_provider.cpp
@@ -348,6 +348,7 @@ std::shared_ptr<DataShare::DataShareHelper> AudioSettingProvider::CreateDataShar
     CHECK_AND_RETURN_RET_LOG(isDataShareReady_.load(), nullptr,
         "DATA_SHARE_READY not received, create DataShareHelper failed");
     if (remoteObj_ == nullptr) {
+        std::lock_guard<std::mutex> lock(mutex_);
         Initialize(AUDIO_POLICY_SERVICE_ID);
     }
 #ifdef SUPPORT_USER_ACCOUNT
