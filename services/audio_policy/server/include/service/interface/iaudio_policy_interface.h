0c302523a545fc909140fb7ae82cda6249fa3876
zhaowenpu <zhaowenpu1@huawei.com>
2025-01-09 07:06:43 +0000

    update services/audio_policy/server/include/service/interface/iaudio_policy_interface.h.
    Signed-off-by: zhaowenpu <zhaowenpu1@huawei.com>

--- services/audio_policy/server/include/service/interface/iaudio_policy_interface.h
+++ services/audio_policy/server/include/service/interface/iaudio_policy_interface.h
@@ -170,7 +170,7 @@ public:
 
     virtual void ResetOffloadSessionId() = 0;
 
-    virtual int32_t SetDoubleRingVolumeDb(AudioStreamType streamType, int32_t volumeLevel) = 0;
+    virtual int32_t SetDoubleRingVolumeDb(const AudioStreamType &streamType, const int32_t &volumeLevel) = 0;
 };
 } // namespace AudioStandard
 } // namespace OHOS
