0f0de9b32ef2a7c82f311ead2f8dec9660c8b2a5
hejunjieru <hejunjie16@h-partners.com>
2025-01-17 11:46:33 +0800

    fix defaut null effectaChain
    Signed-off-by: hejunjieru <hejunjie16@h-partners.com>

--- frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
+++ frameworks/native/audioeffect/src/audio_effect_chain_manager.cpp
@@ -2,7 +2,7 @@
  * Copyright (c) 2023 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License atsceneTypeToEffectChainMap_
+ * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
