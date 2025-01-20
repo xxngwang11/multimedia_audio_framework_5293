xxxppp2025work/multimedia_audio_framework_gitcode

PC多USB需求区分音箱和麦克风
Signed-off-by: 136******92 <yupanwen@huawei.com>
Change-Id: I8f5e32f887c054e97e7b27aa6bf07942be2ecb4b

services/audio_policy/server/src/service/manager/audio_usb_manager.cpp
@@ -169,7 +169,11 @@ void AudioUsbManager::Init(IDeviceStatusObserver *observer)
 {
     lock_guard<mutex> lock(mutex_);
     if (!initialized_) {
-        AUDIO_INFO_LOG("Entry");
+#ifdef PRODUCT_IS_PC
+        AUDIO_INFO_LOG("Entry. PRODUCT_IS_PC=true");
+#else
+        AUDIO_INFO_LOG("Entry. PRODUCT_IS_PC=false");
+#endif
         observer_ = observer;
         RefreshUsbAudioDevices();
         initialized_ = true;

services/audio_service/client/src/audio_device_descriptor.cpp
@@ -172,7 +172,7 @@ bool AudioDeviceDescriptor::Marshalling(Parcel &parcel) const
 
 bool AudioDeviceDescriptor::MarshallingToDeviceDescriptor(Parcel &parcel) const
 {
-    parcel.WriteInt32(MapInternalToExternalDeviceType(deviceType_));
+    parcel.WriteInt32(MapInternalToExternalDeviceType());
     parcel.WriteInt32(deviceRole_);
     parcel.WriteInt32(deviceId_);
     audioStreamInfo_.Marshalling(parcel);
@@ -356,15 +356,25 @@ bool AudioDeviceDescriptor::IsPairedDeviceDesc(const AudioDeviceDescriptor &devi
         deviceDescriptor.networkId_ == networkId_;
 }
 
-DeviceType AudioDeviceDescriptor::MapInternalToExternalDeviceType(DeviceType deviceType)
+DeviceType AudioDeviceDescriptor::MapInternalToExternalDeviceType() const
 {
-    switch (deviceType) {
+    switch (deviceType_) {
+        case DEVICE_TYPE_USB_HEADSET:
         case DEVICE_TYPE_USB_ARM_HEADSET:
+            if (!pairDeviceDescriptor_) {
+#ifdef PRODUCT_IS_PC
+                return DEVICE_TYPE_USB_DEVICE;
+#else
+                if (deviceRole_ == INPUT_DEVICE) {
+                    return DEVICE_TYPE_USB_DEVICE;
+                }
+#endif
+            }
             return DEVICE_TYPE_USB_HEADSET;
         case DEVICE_TYPE_BLUETOOTH_A2DP_IN:
             return DEVICE_TYPE_BLUETOOTH_A2DP;
         default:
-            return deviceType;
+            return deviceType_;
     }
 }
 } // AudioStandard

xxngwang11/multimedia_audio_framework_5293

services/audio_policy/server/src/service/manager/audio_usb_manager.cpp
@@ -169,7 +169,11 @@ void AudioUsbManager::Init(IDeviceStatusObserver *observer)
 {
     lock_guard<mutex> lock(mutex_);
     if (!initialized_) {
-        AUDIO_INFO_LOG("Entry");
+#ifdef DETECT_SOUNDBOX
+        AUDIO_INFO_LOG("Entry. DETECT_SOUNDBOX=true");
+#else
+        AUDIO_INFO_LOG("Entry. DETECT_SOUNDBOX=false");
+#endif
         observer_ = observer;
         RefreshUsbAudioDevices();
         initialized_ = true;

services/audio_service/client/src/audio_device_descriptor.cpp
@@ -172,7 +172,7 @@ bool AudioDeviceDescriptor::Marshalling(Parcel &parcel) const
 
 bool AudioDeviceDescriptor::MarshallingToDeviceDescriptor(Parcel &parcel) const
 {
-    parcel.WriteInt32(MapInternalToExternalDeviceType(deviceType_));
+    parcel.WriteInt32(MapInternalToExternalDeviceType());
     parcel.WriteInt32(deviceRole_);
     parcel.WriteInt32(deviceId_);
     audioStreamInfo_.Marshalling(parcel);
@@ -356,15 +356,25 @@ bool AudioDeviceDescriptor::IsPairedDeviceDesc(const AudioDeviceDescriptor &devi
         deviceDescriptor.networkId_ == networkId_;
 }
 
-DeviceType AudioDeviceDescriptor::MapInternalToExternalDeviceType(DeviceType deviceType)
+DeviceType AudioDeviceDescriptor::MapInternalToExternalDeviceType() const
 {
-    switch (deviceType) {
+    switch (deviceType_) {
+        case DEVICE_TYPE_USB_HEADSET:
         case DEVICE_TYPE_USB_ARM_HEADSET:
+            if (!pairDeviceDescriptor_) {
+#ifdef DETECT_SOUNDBOX
+                return DEVICE_TYPE_USB_DEVICE;
+#else
+                if (deviceRole_ == INPUT_DEVICE) {
+                    return DEVICE_TYPE_USB_DEVICE;
+                }
+#endif
+            }
             return DEVICE_TYPE_USB_HEADSET;
         case DEVICE_TYPE_BLUETOOTH_A2DP_IN:
             return DEVICE_TYPE_BLUETOOTH_A2DP;
         default:
-            return deviceType;
+            return deviceType_;
     }
 }
 } // AudioStandard