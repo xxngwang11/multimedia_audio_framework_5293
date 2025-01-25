xxxppp2025work/multimedia_audio_framework_gitcode

audio server 解耦 audio client
Signed-off-by: zhangwt3652 <zhangwenting15@huawei.com>

services/audio_service/client/src/audio_stream_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -23,6 +23,7 @@
 #include "audio_policy_manager.h"
 #include "audio_utils.h"
 #include "i_audio_stream.h"
+#include "audio_util.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -139,7 +140,7 @@ static void UpdateEffectInfoArray(SupportedEffectConfig &supportedEffectConfig,
 
 int32_t AudioStreamManager::GetEffectInfoArray(AudioSceneEffectInfo &audioSceneEffectInfo, StreamUsage streamUsage)
 {
-    std::string effectScene = IAudioStream::GetEffectSceneName(streamUsage);
+    std::string effectScene = AudioUtil::GetEffectSceneName(streamUsage);
     SupportedEffectConfig supportedEffectConfig;
     int32_t ret = AudioPolicyManager::GetInstance().QueryEffectSceneMode(supportedEffectConfig);
     uint32_t streamNum = supportedEffectConfig.postProcessNew.stream.size();

services/audio_service/client/src/renderer_in_client_public.cpp
@@ -52,6 +52,7 @@
 #include "audio_spatialization_manager.h"
 #include "policy_handler.h"
 #include "volume_tools.h"
+#include "audio_util.h"
 
 #include "media_monitor_manager.h"
 
@@ -181,7 +182,7 @@ void RendererInClientInner::SetRendererInfo(const AudioRendererInfo &rendererInf
 {
     rendererInfo_ = rendererInfo;
 
-    rendererInfo_.sceneType = GetEffectSceneName(rendererInfo_.streamUsage);
+    rendererInfo_.sceneType = AudioUtil::GetEffectSceneName(rendererInfo_.streamUsage);
 
     if (rendererInfo_.sceneType == AUDIO_SUPPORTED_SCENE_TYPES.find(SCENE_OTHERS)->second) {
         effectMode_ = EFFECT_NONE;

xxngwang11/multimedia_audio_framework_5293

services/audio_service/client/src/audio_stream_manager.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2025 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -20,6 +20,7 @@
 
 #include "audio_errors.h"
 #include "audio_common_log.h"
+#include "audio_manager_util.h"
 #include "audio_policy_manager.h"
 #include "audio_utils.h"
 #include "i_audio_stream.h"
@@ -139,7 +140,7 @@ static void UpdateEffectInfoArray(SupportedEffectConfig &supportedEffectConfig,
 
 int32_t AudioStreamManager::GetEffectInfoArray(AudioSceneEffectInfo &audioSceneEffectInfo, StreamUsage streamUsage)
 {
-    std::string effectScene = IAudioStream::GetEffectSceneName(streamUsage);
+    std::string effectScene = AudioManagerUtil::GetEffectSceneName(streamUsage);
     SupportedEffectConfig supportedEffectConfig;
     int32_t ret = AudioPolicyManager::GetInstance().QueryEffectSceneMode(supportedEffectConfig);
     uint32_t streamNum = supportedEffectConfig.postProcessNew.stream.size();

services/audio_service/client/src/renderer_in_client_public.cpp
@@ -52,6 +52,7 @@
 #include "audio_spatialization_manager.h"
 #include "policy_handler.h"
 #include "volume_tools.h"
+#include "audio_manager_util.h"
 
 #include "media_monitor_manager.h"
 
@@ -181,7 +182,7 @@ void RendererInClientInner::SetRendererInfo(const AudioRendererInfo &rendererInf
 {
     rendererInfo_ = rendererInfo;
 
-    rendererInfo_.sceneType = GetEffectSceneName(rendererInfo_.streamUsage);
+    rendererInfo_.sceneType = AudioManagerUtil::GetEffectSceneName(rendererInfo_.streamUsage);
 
     if (rendererInfo_.sceneType == AUDIO_SUPPORTED_SCENE_TYPES.find(SCENE_OTHERS)->second) {
         effectMode_ = EFFECT_NONE;