f22dbbe719cf171441a089cf9ff277ad427ec4fa
hejunjieru <hejunjie16@h-partners.com>
2025-02-06 09:42:39 +0800

    fix send effectMode
    Signed-off-by: hejunjieru <hejunjie16@h-partners.com>

--- services/audio_service/server/src/pa_adapter_manager.cpp
+++ services/audio_service/server/src/pa_adapter_manager.cpp
@@ -469,11 +469,9 @@ void PaAdapterManager::ReleasePaStream(pa_stream *paStream)
     AUDIO_INFO_LOG("Release paStream because of errs");
 }
 
-bool PaAdapterManager::IsEffectNone(StreamUsage streamUsage)
+bool PaAdapterManager::IsEffectNone(int32_t effectMode)
 {
-    if (streamUsage == STREAM_USAGE_SYSTEM || streamUsage == STREAM_USAGE_DTMF ||
-        streamUsage == STREAM_USAGE_ENFORCED_TONE || streamUsage == STREAM_USAGE_ULTRASONIC ||
-        streamUsage == STREAM_USAGE_NAVIGATION || streamUsage == STREAM_USAGE_NOTIFICATION) {
+    if (effectMode == EFFECT_NONE) {
         return true;
     }
     return false;
@@ -516,7 +514,7 @@ void PaAdapterManager::SetHighResolution(pa_proplist *propList, AudioProcessConf
 void PaAdapterManager::SetPlaybackProplist(pa_proplist *propList, AudioProcessConfig &processConfig)
 {
         pa_proplist_sets(propList, "scene.mode",
-            IsEffectNone(processConfig.rendererInfo.streamUsage) ? "EFFECT_NONE" : "EFFECT_DEFAULT");
+            IsEffectNone(processConfig.rendererInfo.effectMode) ? "EFFECT_NONE" : "EFFECT_DEFAULT");
         // mark dup stream for dismissing volume handle
         std::string streamMode = managerType_ == DUP_PLAYBACK ? DUP_STREAM
             : (managerType_ == DUAL_PLAYBACK ? DUAL_TONE_STREAM : NORMAL_STREAM);
