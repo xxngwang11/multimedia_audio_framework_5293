c1eba056012fce8262dde5667fb859f2c4428f66
Wang Chongteng <wangchongteng@huawei.com>
2025-02-22 10:48:01 +0800

    音频框架提升优先级改为异步，防止阻塞开机
    Signed-off-by: Wang Chongteng <wangchongteng@huawei.com>

--- frameworks/native/audioqosmanager/audio_qosmanager.cpp
+++ frameworks/native/audioqosmanager/audio_qosmanager.cpp
@@ -37,7 +37,7 @@ extern "C" {
 
 #ifdef QOSMANAGER_ENABLE
 const std::string BOOT_ANIMATION_FINISHED_EVENT = "bootevent.bootanimation.finished";
-constexpr int32_t WAIT_FOR_BOOT_ANIMATION_S = 10;
+constexpr int32_t WAIT_FOR_BOOT_ANIMATION_S = 14;
 constexpr int32_t WAIT_TIME_FOR_UNSCHEDULE_MS = 100;
 constexpr int32_t WAIT_COUNT_FOR_UNSCHEDULE = 5;
 void SetThreadQosLevel(void)
