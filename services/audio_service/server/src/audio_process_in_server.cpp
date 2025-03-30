a49d36f255a8ee2d18406e43738ce610f7c035b4
Jvgang <yujugang2@h-partners.com>
2025-03-30 16:48:40 +0000

    Add micLight state for Capture Stream
    Signed-off-by: Jvgang <yujugang2@h-partners.com>

--- services/audio_service/server/src/audio_process_in_server.cpp
+++ services/audio_service/server/src/audio_process_in_server.cpp
@@ -247,7 +247,7 @@ int32_t AudioProcessInServer::StartInner()
     }
 
     if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
-        CHECK_AND_RETURENRET_LOG(TurnOnMicLight(CAPTURER_RUNNING), false,
+        CHECK_AND_RETURENRET_LOG(TurnOnMicLight(CAPTURER_RUNNING), ERR_PERMISSION_DENIED,
             "Turn on micLight failed or check backgroud capture failed for stream:%{public}d!", sessionId_);
     }
 
@@ -311,7 +311,7 @@ int32_t AudioProcessInServer::Resume()
         needCheckBackground_ = true;
     }
     if (processConfig_.audioMode == AUDIO_MODE_RECORD && needCheckBackground_) {
-        CHECK_AND_RETURENRET_LOG(TurnOnMicLight(CAPTURER_RUNNING), false,
+        CHECK_AND_RETURENRET_LOG(TurnOnMicLight(CAPTURER_RUNNING), ERR_PERMISSION_DENIED,
             "Turn on micLight failed or check backgroud capture failed for stream:%{public}d!", sessionId_);
     }
 
