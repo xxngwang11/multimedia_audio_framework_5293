49277e849b4bffa0e9719cd6ee057543a433e908
c00657214 <chenxin666@huawei.com>
2025-04-19 12:01:07 +0800

    fix CODECHECK 7
    Signed-off-by: c00657214 <chenxin666@huawei.com>

--- services/audio_engine/plugin/channel_converter/src/down_mixer.cpp
+++ services/audio_engine/plugin/channel_converter/src/down_mixer.cpp
@@ -203,12 +203,15 @@ void DownMixer::NormalizeDMixTable()
         }
         maxx = std::max(maxx, summ);
     }
-    if (maxx == 0.0f) {
+
+    if (maxx != 0.0f) {
+        maxx = 1.0f / maxx;
+    } else {
         AUDIO_ERR_LOG("invalid channel num: in_ch = %{public}u, out_ch = %{public}u",
                 inChannels_, outChannels_);
         maxx = 1.0f;
     }
-    maxx = 1.0f / maxx;
+
     for (uint32_t i = 0; i < outChannels_; i++) {
         for (uint32_t j = 0; j < inChannels_; j++) {
             downMixTable_[i][j] *= maxx;
