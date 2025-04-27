a862faf87d3d710c3838e8a98193e1fa6de6c6e0
Charles <wangchao685@h-partners.com>
2025-04-27 14:28:51 +0800

    ProAudio UT、录制信息随数据上报
    Signed-off-by: Charles <wangchao685@h-partners.com>

--- services/audio_engine/manager/src/hpae_manager_impl.cpp
+++ services/audio_engine/manager/src/hpae_manager_impl.cpp
@@ -236,7 +236,8 @@ int32_t HpaeManagerImpl::RegisterStatusCallback(HpaeStreamClassType streamClassT
 }
 
 // record stream interface
-int32_t HpaeManagerImpl::RegisterReadCallback(uint32_t sessionId, const std::weak_ptr<ICapturerStreamCallback> &callback)
+int32_t HpaeManagerImpl::RegisterReadCallback(uint32_t sessionId,
+    const std::weak_ptr<ICapturerStreamCallback> &callback)
 {
     CHECK_AND_RETURN_RET_LOG(manager_, ERR_ILLEGAL_STATE, "manager is nullptr");
     return manager_->RegisterReadCallback(sessionId, callback);
