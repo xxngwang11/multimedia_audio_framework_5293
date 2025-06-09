336f66649c8a07e212edcf403e7dd84ae82f3618
蒋志庆 <jiangzhiqing4@h-partners.com>
2025-06-09 17:27:08 +0800

    bugfix
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- frameworks/native/audioloopback/src/audio_loopback.cpp
+++ frameworks/native/audioloopback/src/audio_loopback.cpp
@@ -174,7 +174,7 @@ void AudioLoopbackPrivate::DisableLoopback()
     karaokeParams_["Karaoke_enable"] = "disable";
     std::string parameters = "Karaoke_enable=" + karaokeParams_["Karaoke_enable"];
     CHECK_AND_RETURN_LOG(AudioPolicyManager::GetInstance().SetKaraokeParameters(parameters),
-            "DisableLoopback failed");
+        "DisableLoopback failed");
  }
 
 void AudioLoopbackPrivate::DestroyAudioLoopback()
