a543c7a571e995a946d39f550f372357ea53e88e
zhangwenting <zhangwenting15@huawei.com>
2025-06-13 17:48:24 +0800

    add audio framework fuzz
    Signed-off-by: zhangwenting <zhangwenting15@huawei.com>

--- test/fuzztest/audiovolumemanager_fuzzer/audio_volume_manager_fuzz.cpp
+++ test/fuzztest/audiovolumemanager_fuzzer/audio_volume_manager_fuzz.cpp
@@ -375,6 +375,22 @@ void AudioVolumeManagerGetAllDeviceVolumeInfoFuzzTest(const uint8_t *rawData, si
     audioVolumeManager->GetAllDeviceVolumeInfo();
 }
 
+void AudioVolumeManagerInitFuzzTest(const uint8_t *rawData, size_t size)
+{
+    AudioVolumeManager& audioVolumeManager(AudioVolumeManager::GetInstance());
+    std::shared_ptr<AudioPolicyServerHandler> audioPolicyServerHandler = std::make_shared<AudioPolicyServerHandler>();
+
+    audioVolumeManager.DeInit();
+    audioVolumeManager.Init(audioPolicyServerHandler);
+}
+
+void AudioVolumeManagerInitKVStoreFuzzTest(const uint8_t *rawData, size_t size)
+{
+    AudioVolumeManager& audioVolumeManager(AudioVolumeManager::GetInstance());
+
+    audioVolumeManager.InitKVStore();
+}
+
 } // namespace AudioStandard
 } // namesapce OHOS
 
@@ -399,6 +415,8 @@ OHOS::AudioStandard::TestPtr g_testPtrs[] = {
     OHOS::AudioStandard::AudioVolumeManagerGetMaxVolumeLevelFuzzTest,
     OHOS::AudioStandard::AudioVolumeManagerGetMinVolumeLevelFuzzTest,
     OHOS::AudioStandard::AudioVolumeManagerGetAllDeviceVolumeInfoFuzzTest,
+    OHOS::AudioStandard::AudioVolumeManagerInitFuzzTest,
+    OHOS::AudioStandard::AudioVolumeManagerInitKVStoreFuzzTest,
 };
 
 /* Fuzzer entry point */
