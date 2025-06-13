xxxppp2025work/multimedia_audio_framework_gitcode

add Eploop Thread to perf domain
Signed-off-by: wx956695 <libin242@h-partners.com>

frameworks/native/audioschedule/audio_schedule.cpp
@@ -18,12 +18,16 @@
 
 #include "audio_schedule.h"
 #include "audio_schedule_guard.h"
+#include "parameter.h"
 
 #include <unistd.h>
 #include <sys/types.h>
 #include <cstring>
 #include <unordered_map>
 #include <set>
+#include <pthread.h>
+#include <sched.h>
+#include <thread>
 
 #ifdef RESSCHE_ENABLE
 #include "res_type.h"
@@ -44,6 +48,9 @@ const uint32_t AUDIO_QOS_LEVEL = 7;
 const int32_t DEFAULT_QOS_LEVEL = -1;
 const uint32_t REPORTDATA_TIMEOUT = 8;
 const int32_t HIGH_LEVEL_THREAD_PRIORITY = 4;
+const int32_t ADUIO_DEFAULT_PRIORITY = 1;
+const int32_t MAX_RETRY_TIMES = 5;
+const int32_t WAIT_TIME_FOR_UNSCHEDULE_MS = 5;
 static std::mutex g_rssMutex;
 static std::set<uint32_t> g_tidToReport = {};
 constexpr uint32_t g_type = OHOS::ResourceSchedule::ResType::RES_TYPE_THREAD_QOS_CHANGE;
@@ -142,7 +149,23 @@ void OnAddResSchedService(uint32_t audioServerPid)
 
 bool SetEndpointThreadPriority()
 {
+    Trace trace("SetEndpointThreadPriority");
+    int32_t perfdomain = GetIntParameter("const.multimedia.audio_setPriority", ADUIO_DEFAULT_PRIORITY);
+    AUDIO_INFO_LOG("perfdomain = %{public}d", perfdomain);
     struct sched_param param = {0};
+    // add to perfdomain
+    if (perfdomain > ADUIO_DEFAULT_PRIORITY) {
+        ScheduleReportData(getpid(), gettid(), "audio_server");
+        int32_t policy = 0;
+        int32_t cnt = 0;
+        while ((pthread_getschedparam(pthread_self(), &policy, &param) == 0) && (cnt++ < MAX_RETRY_TIMES)) {
+            if (policy == SCHED_RR || policy == SCHED_FIFO) {
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_TIME_FOR_UNSCHEDULE_MS));
+        }
+        AUDIO_INFO_LOG("RETRY_TIMES = %{public}d", cnt);
+    }
     // priority 50 + 4 = 54
     param.sched_priority = HIGH_LEVEL_THREAD_PRIORITY;
     auto res = sched_setscheduler(0, SCHED_FIFO | SCHED_RESET_ON_FORK, &param);

xxngwang11/multimedia_audio_framework_5293

frameworks/native/audioschedule/audio_schedule.cpp
@@ -24,6 +24,10 @@
 #include <cstring>
 #include <unordered_map>
 #include <set>
+#include <pthread.h>
+#include <sched.h>
+#include <thread>
+#include "parameter.h"
 
 #ifdef RESSCHE_ENABLE
 #include "res_type.h"
@@ -44,6 +48,9 @@ const uint32_t AUDIO_QOS_LEVEL = 7;
 const int32_t DEFAULT_QOS_LEVEL = -1;
 const uint32_t REPORTDATA_TIMEOUT = 8;
 const int32_t HIGH_LEVEL_THREAD_PRIORITY = 4;
+const int32_t AUDIO_DEFAULT_PRIORITY = 1;
+const int32_t MAX_RETRY_TIMES = 5;
+const int32_t WAIT_TIME_FOR_UNSCHEDULE_MS = 5;
 static std::mutex g_rssMutex;
 static std::set<uint32_t> g_tidToReport = {};
 constexpr uint32_t g_type = OHOS::ResourceSchedule::ResType::RES_TYPE_THREAD_QOS_CHANGE;
@@ -142,7 +149,23 @@ void OnAddResSchedService(uint32_t audioServerPid)
 
 bool SetEndpointThreadPriority()
 {
+    Trace trace("SetEndpointThreadPriority");
+    int32_t perfdomain = GetIntParameter("const.multimedia.audio_setPriority", AUDIO_DEFAULT_PRIORITY);
+    AUDIO_INFO_LOG("perfdomain = %{public}d", perfdomain);
     struct sched_param param = {0};
+    // add to perfdomain
+    if (perfdomain > AUDIO_DEFAULT_PRIORITY) {
+        ScheduleReportData(getpid(), gettid(), "audio_server");
+        int32_t policy = 0;
+        int32_t cnt = 0;
+        while ((pthread_getschedparam(pthread_self(), &policy, &param) == 0) && (cnt++ < MAX_RETRY_TIMES)) {
+            if (policy == SCHED_RR || policy == SCHED_FIFO) {
+                break;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(WAIT_TIME_FOR_UNSCHEDULE_MS));
+        }
+        AUDIO_INFO_LOG("RETRY_TIMES = %{public}d", cnt);
+    }
     // priority 50 + 4 = 54
     param.sched_priority = HIGH_LEVEL_THREAD_PRIORITY;
     auto res = sched_setscheduler(0, SCHED_FIFO | SCHED_RESET_ON_FORK, &param);