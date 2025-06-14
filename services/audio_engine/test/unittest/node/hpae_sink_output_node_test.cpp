0c2f41cd1fd775d5e97ab8ffea8baab814bb84a9
guoyao <guoyao18@huawei.com>
2025-06-14 18:01:02 +0800

    proaudio单元测试拆分
    Signed-off-by: guoyao <guoyao18@huawei.com>

--- services/audio_engine/test/unittest/node/hpae_sink_output_node_test.cpp
+++ services/audio_engine/test/unittest/node/hpae_sink_output_node_test.cpp
@@ -23,7 +23,7 @@
 namespace OHOS {
 namespace AudioStandard {
 namespace HPAE {
-const char *g_rootPath = "/data/source_file_io_48000_2_s16le.pcm"
+const char *ROOT_PATH = "/data/source_file_io_48000_2_s16le.pcm"
 class HpaeSinkOutputNodeTest : public testing::Test {
 public:
     void SetUp();
@@ -100,7 +100,7 @@ TEST_F(HpaeSinkOutputNodeTest, testHpaeSinkOutConnectNode)
     attr.sampleRate = nodeInfo.samplingRate;
     attr.channel = nodeInfo.channels;
     attr.volume = 0.0f;
-    attr.filePath = g_rootPath;
+    attr.filePath = ROOT_PATH;
     attr.deviceNetworkId = deviceNetId.c_str();
     attr.deviceType = 0;
     attr.channelLayout = 0;
@@ -145,7 +145,7 @@ TEST_F(HpaeSinkOutputNodeTest, testHpaeSinkOutConnectNodeRemote)
     attr.sampleRate = nodeInfo.samplingRate;
     attr.channel = nodeInfo.channels;
     attr.volume = 0.0f;
-    attr.filePath = g_rootPath;
+    attr.filePath = ROOT_PATH;
     attr.deviceNetworkId = deviceNetId.c_str();
     attr.deviceType = 0;
     attr.channelLayout = 0;
@@ -190,7 +190,7 @@ TEST_F(HpaeSinkOutputNodeTest, testHpaeSinkOutHandlePaPower)
     attr.sampleRate = nodeInfo.samplingRate;
     attr.channel = nodeInfo.channels;
     attr.volume = 0.0f;
-    attr.filePath = g_rootPath;
+    attr.filePath = ROOT_PATH;
     attr.deviceNetworkId = deviceNetId.c_str();
     attr.deviceType = 0;
     attr.channelLayout = 0;
