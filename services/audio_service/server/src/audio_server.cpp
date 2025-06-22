0147b975656018bd9ed381d2f6b8d87b866cb12e
Gisaac <gongjie13@huawei.com>
2025-06-22 17:47:50 +0800

    set scene and device for fast
    Signed-off-by: Gisaac <gongjie13@huawei.com>

--- services/audio_service/server/src/audio_server.cpp
+++ services/audio_service/server/src/audio_server.cpp
@@ -225,6 +225,45 @@ static void UpdateArmInstance(std::shared_ptr<IAudioRenderSink> &sink,
     primarySink->ResetActiveDeviceForDisconnect(DEVICE_TYPE_NONE);
 }
 
+static void SetAudioSceneForAllSource(std::shared_ptr<IAudioCaptureSource> &source,
+    AudioScene audioScene, DeviceType activeInputDevice)
+{
+    if (source == nullptr || !source->IsInited()) {
+        AUDIO_WARNING_LOG("Capturer is not initialized.");
+    } else {
+        source->SetAudioScene(audioScene, activeInputDevice);
+    }
+#ifdef SUPPORT_LOW_LATENCY
+    std::shared_ptr<IAudioCaptureSource> fastSource = GetSourceByProp(HDI_ID_TYPE_FAST, HDI_ID_INFO_DEFAULT, true);
+    if (fastSource != nullptr && fastSource->IsInited()) {
+        fastSource->SetAudioScene(audioScene, activeInputDevice);
+    }
+    std::shared_ptr<IAudioCaptureSource> fastVoipSource = GetSourceByProp(HDI_ID_TYPE_FAST, HDI_ID_INFO_VOIP, true);
+    if (fastVoipSource != nullptr && fastVoipSource->IsInited()) {
+        fastVoipSource->SetAudioScene(audioScene, activeInputDevice);
+    }
+#endif
+}
+
+static void UpdateDeviceForAllSource(std::shared_ptr<IAudioCaptureSource> &source, DeviceType type)
+{
+    if (source == nullptr || !source->IsInited()) {
+        AUDIO_WARNING_LOG("Capturer is not initialized.");
+    } else {
+        source->UpdateActiveDevice(type);
+    }
+#ifdef SUPPORT_LOW_LATENCY
+    std::shared_ptr<IAudioCaptureSource> fastSource = GetSourceByProp(HDI_ID_TYPE_FAST, HDI_ID_INFO_DEFAULT, true);
+    if (fastSource != nullptr && fastSource->IsInited()) {
+        fastSource->SetAudioScene(audioScene, activeInputDevice);
+    }
+    std::shared_ptr<IAudioCaptureSource> fastVoipSource = GetSourceByProp(HDI_ID_TYPE_FAST, HDI_ID_INFO_VOIP, true);
+    if (fastVoipSource != nullptr && fastVoipSource->IsInited()) {
+        fastVoipSource->SetAudioScene(audioScene, activeInputDevice);
+    }
+#endif
+}
+
 static void UpdatePrimaryInstance(std::shared_ptr<IAudioRenderSink> &sink,
     std::shared_ptr<IAudioCaptureSource> &source)
 {
@@ -1112,15 +1151,7 @@ int32_t AudioServer::SetAudioScene(AudioScene audioScene, std::vector<DeviceType
         source = GetSourceByProp(HDI_ID_TYPE_PRIMARY);
     }
 
-    if (source == nullptr || !source->IsInited()) {
-        AUDIO_WARNING_LOG("Capturer is not initialized.");
-    } else {
-        source->SetAudioScene(audioScene, activeInputDevice);
-    }
-    std::shared_ptr<IAudioCaptureSource> fastSource = GetSourceByProp(HDI_ID_TYPE_FAST, HDI_ID_INFO_DEFAULT, true);
-    if (fastSource != nullptr && fastSource->IsInited()) {
-        fastSource->SetAudioScene(audioScene, activeInputDevice);
-    }
+    SetAudioSceneForAllSource(source, audioScene, activeInputDevice);
     if (sink == nullptr || !sink->IsInited()) {
         AUDIO_WARNING_LOG("Renderer is not initialized.");
     } else {
@@ -1157,16 +1188,15 @@ int32_t AudioServer::SetIORoutes(std::vector<std::pair<DeviceType, DeviceFlag>> 
 int32_t AudioServer::SetIORoutes(DeviceType type, DeviceFlag flag, std::vector<DeviceType> deviceTypes,
     BluetoothOffloadState a2dpOffloadFlag, const std::string &deviceName)
 {
-    std::shared_ptr<IAudioRenderSink> sink = nullptr;
+    std::shared_ptr<IAudioRenderSink> sink = GetSinkByProp(HDI_ID_TYPE_PRIMARY, HDI_ID_INFO_DEFAULT, true);
     std::shared_ptr<IAudioCaptureSource> source = nullptr;
 
     if (type == DEVICE_TYPE_USB_ARM_HEADSET) {
         UpdateArmInstance(sink, source);
     } else if (type == DEVICE_TYPE_ACCESSORY) {
-        sink = GetSinkByProp(HDI_ID_TYPE_PRIMARY, HDI_ID_INFO_DEFAULT, true);
         source = GetSourceByProp(HDI_ID_TYPE_ACCESSORY, HDI_ID_INFO_ACCESSORY, true);
     } else {
-        UpdatePrimaryInstance(sink, source);
+        source = GetSourceByProp(HDI_ID_TYPE_PRIMARY, HDI_ID_INFO_DEFAULT, true);
         if (type == DEVICE_TYPE_BLUETOOTH_A2DP && a2dpOffloadFlag != A2DP_OFFLOAD) {
             deviceTypes[0] = DEVICE_TYPE_NONE;
         }
@@ -1177,9 +1207,9 @@ int32_t AudioServer::SetIORoutes(DeviceType type, DeviceFlag flag, std::vector<D
     std::lock_guard<std::mutex> lock(audioSceneMutex_);
     if (flag == DeviceFlag::INPUT_DEVICES_FLAG) {
         if (audioScene_ != AUDIO_SCENE_DEFAULT) {
-            source->SetAudioScene(audioScene_, type);
+            SetAudioSceneForAllSource(source, audioScene_, type);
         } else {
-            source->UpdateActiveDevice(type);
+            UpdateDeviceForAllSource(source, type);
         }
     } else if (flag == DeviceFlag::OUTPUT_DEVICES_FLAG) {
         if (audioScene_ != AUDIO_SCENE_DEFAULT) {
@@ -1190,10 +1220,10 @@ int32_t AudioServer::SetIORoutes(DeviceType type, DeviceFlag flag, std::vector<D
         PolicyHandler::GetInstance().SetActiveOutputDevice(type);
     } else if (flag == DeviceFlag::ALL_DEVICES_FLAG) {
         if (audioScene_ != AUDIO_SCENE_DEFAULT) {
-            source->SetAudioScene(audioScene_, type);
+            SetAudioSceneForAllSource(source, audioScene_, type);
             sink->SetAudioScene(audioScene_, deviceTypes);
         } else {
-            source->UpdateActiveDevice(type);
+            UpdateDeviceForAllSource(source, type);
             sink->UpdateActiveDevice(deviceTypes);
         }
         PolicyHandler::GetInstance().SetActiveOutputDevice(type);
