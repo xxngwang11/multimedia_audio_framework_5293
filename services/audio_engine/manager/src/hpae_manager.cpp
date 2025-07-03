390270d09944cb3801700878c8079f378ce4ba3a
Maninblack <shanliang6@huawei.com>
2025-07-03 14:56:30 +0800

    delete redundant call
    Signed-off-by: Maninblack <shanliang6@huawei.com>

--- services/audio_engine/manager/src/hpae_manager.cpp
+++ services/audio_engine/manager/src/hpae_manager.cpp
@@ -2082,8 +2082,11 @@ int32_t HpaeManager::SetAudioEnhanceProperty(const AudioEffectPropertyArrayV3 &p
 
 int32_t HpaeManager::GetAudioEnhanceProperty(AudioEffectPropertyArrayV3 &propertyArray, DeviceType deviceType)
 {
-    auto request = [&propertyArray, deviceType]() {
+    auto request = [this, &propertyArray, deviceType]() {
         HpaePolicyManager::GetInstance().GetAudioEnhanceProperty(propertyArray, deviceType);
+        if (auto serviceCallback = serviceCallback_.lock()) {
+            serviceCallback->OnGetAudioEnhancePropertyCbV3(SUCCESS);
+        }
     };
     SendRequest(request, __func__);
     return SUCCESS;
