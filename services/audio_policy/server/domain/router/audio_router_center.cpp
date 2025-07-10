xxxppp2025work/multimedia_audio_framework_gitcode

单应用投屏bugfix
Signed-off-by: darkshadow <chenyan218@huawei.com>

services/audio_policy/server/domain/router/audio_router_center.cpp
@@ -220,11 +220,7 @@ std::vector<std::shared_ptr<AudioDeviceDescriptor>> AudioRouterCenter::FetchOutp
 {
     vector<shared_ptr<AudioDeviceDescriptor>> descs;
     RouterType routerType = ROUTER_TYPE_NONE;
-    int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(clientUID);
-    if (zoneId != 0) {
-        return AudioZoneService::GetInstance().FetchOutputDevices(zoneId, streamUsage, clientUID, routerType);
-    }
-    zoneId = AudioZoneService::GetInstance().FindAudioZoneByUsage(streamUsage);
+    int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUidOrUsage(clientUID, streamUsage);
     if (zoneId != 0) {
         return AudioZoneService::GetInstance().FetchOutputDevices(zoneId, streamUsage, clientUID, routerType);
     }

services/audio_policy/server/domain/zone/include/audio_zone_service.h
@@ -56,7 +56,7 @@ public:
     int32_t AddStreamToAudioZone(int32_t zoneId, AudioZoneStream stream);
     int32_t RemoveStreamFromAudioZone(int32_t zoneId, AudioZoneStream stream);
     int32_t FindAudioZoneByUid(int32_t uid);
-    int32_t FindAudioZoneByUsage(StreamUsage usage);
+    int32_t FindAudioZoneByUidOrUsage(int32_t uid, StreamUsage usage);
 
     int32_t EnableSystemVolumeProxy(pid_t clientPid, int32_t zoneId, bool enable);
     bool IsSystemVolumeProxyEnable(int32_t zoneId);

services/audio_policy/server/domain/zone/src/audio_zone_service.cpp
@@ -289,10 +289,11 @@ int32_t AudioZoneService::FindAudioZoneByUid(int32_t uid)
     return FindAudioZoneByKey(uid, "", "", StreamUsage::STREAM_USAGE_INVALID);
 }
 
-int32_t AudioZoneService::FindAudioZoneByUsage(StreamUsage usage)
+int32_t AudioZoneService::FindAudioZoneByUidOrUsage(int32_t uid, StreamUsage usage)
 {
     std::lock_guard<std::mutex> lock(zoneMutex_);
-    return FindAudioZoneByKey(INVALID_ZONEID, "", "", usage);
+    int32_t zoneId = FindAudioZoneByKey(uid, "", "", StreamUsage::STREAM_USAGE_INVALID);
+    return zoneId != 0 ? zoneId : FindAudioZoneByKey(INVALID_ZONEID, "", "", usage);
 }
 
 int32_t AudioZoneService::FindAudioZoneByKey(int32_t uid, const std::string &deviceTag,

services/audio_policy/server/service/service_main/src/audio_policy_server.cpp
@@ -2297,7 +2297,8 @@ int32_t AudioPolicyServer::ActivateAudioInterrupt(
                 audioInterrupt.sessionStrategy.concurrencyMode = AudioConcurrencyMode::SILENT;
             }
         }
-        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(IPCSkeleton::GetCallingUid());
+        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUidOrUsage(audioInterrupt.uid,
+            audioInterrupt.streamUsage);
         return AudioZoneService::GetInstance().ActivateAudioInterrupt(zoneId, audioInterrupt, isUpdatedAudioStrategy);
     }
     return ERR_UNKNOWN;
@@ -2306,7 +2307,8 @@ int32_t AudioPolicyServer::ActivateAudioInterrupt(
 int32_t AudioPolicyServer::DeactivateAudioInterrupt(const AudioInterrupt &audioInterrupt, const int32_t zoneID)
 {
     if (interruptService_ != nullptr) {
-        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(IPCSkeleton::GetCallingUid());
+        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUidOrUsage(audioInterrupt.uid,
+            audioInterrupt.streamUsage);
         return AudioZoneService::GetInstance().DeactivateAudioInterrupt(zoneId, audioInterrupt);
     }
     return ERR_UNKNOWN;

xxngwang11/multimedia_audio_framework_5293

services/audio_policy/server/domain/router/audio_router_center.cpp
@@ -220,11 +220,7 @@ std::vector<std::shared_ptr<AudioDeviceDescriptor>> AudioRouterCenter::FetchOutp
 {
     vector<shared_ptr<AudioDeviceDescriptor>> descs;
     RouterType routerType = ROUTER_TYPE_NONE;
-    int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(clientUID);
-    if (zoneId != 0) {
-        return AudioZoneService::GetInstance().FetchOutputDevices(zoneId, streamUsage, clientUID, routerType);
-    }
-    zoneId = AudioZoneService::GetInstance().FindAudioZoneByUsage(streamUsage);
+    int32_t zoneId = AudioZoneService::GetInstance().FindAudioZone(clientUID, streamUsage);
     if (zoneId != 0) {
         return AudioZoneService::GetInstance().FetchOutputDevices(zoneId, streamUsage, clientUID, routerType);
     }

services/audio_policy/server/domain/zone/include/audio_zone_service.h
@@ -56,7 +56,7 @@ public:
     int32_t AddStreamToAudioZone(int32_t zoneId, AudioZoneStream stream);
     int32_t RemoveStreamFromAudioZone(int32_t zoneId, AudioZoneStream stream);
     int32_t FindAudioZoneByUid(int32_t uid);
-    int32_t FindAudioZoneByUsage(StreamUsage usage);
+    int32_t FindAudioZone(int32_t uid, StreamUsage usage);
 
     int32_t EnableSystemVolumeProxy(pid_t clientPid, int32_t zoneId, bool enable);
     bool IsSystemVolumeProxyEnable(int32_t zoneId);

services/audio_policy/server/domain/zone/src/audio_zone_service.cpp
@@ -289,10 +289,11 @@ int32_t AudioZoneService::FindAudioZoneByUid(int32_t uid)
     return FindAudioZoneByKey(uid, "", "", StreamUsage::STREAM_USAGE_INVALID);
 }
 
-int32_t AudioZoneService::FindAudioZoneByUsage(StreamUsage usage)
+int32_t AudioZoneService::FindAudioZone(int32_t uid, StreamUsage usage)
 {
     std::lock_guard<std::mutex> lock(zoneMutex_);
-    return FindAudioZoneByKey(INVALID_ZONEID, "", "", usage);
+    int32_t zoneId = FindAudioZoneByKey(uid, "", "", StreamUsage::STREAM_USAGE_INVALID);
+    return zoneId != 0 ? zoneId : FindAudioZoneByKey(INVALID_ZONEID, "", "", usage);
 }
 
 int32_t AudioZoneService::FindAudioZoneByKey(int32_t uid, const std::string &deviceTag,

services/audio_policy/server/service/service_main/src/audio_policy_server.cpp
@@ -2297,7 +2297,8 @@ int32_t AudioPolicyServer::ActivateAudioInterrupt(
                 audioInterrupt.sessionStrategy.concurrencyMode = AudioConcurrencyMode::SILENT;
             }
         }
-        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(IPCSkeleton::GetCallingUid());
+        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZone(audioInterrupt.uid,
+            audioInterrupt.streamUsage);
         return AudioZoneService::GetInstance().ActivateAudioInterrupt(zoneId, audioInterrupt, isUpdatedAudioStrategy);
     }
     return ERR_UNKNOWN;
@@ -2306,7 +2307,8 @@ int32_t AudioPolicyServer::ActivateAudioInterrupt(
 int32_t AudioPolicyServer::DeactivateAudioInterrupt(const AudioInterrupt &audioInterrupt, const int32_t zoneID)
 {
     if (interruptService_ != nullptr) {
-        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZoneByUid(IPCSkeleton::GetCallingUid());
+        int32_t zoneId = AudioZoneService::GetInstance().FindAudioZone(audioInterrupt.uid,
+            audioInterrupt.streamUsage);
         return AudioZoneService::GetInstance().DeactivateAudioInterrupt(zoneId, audioInterrupt);
     }
     return ERR_UNKNOWN;