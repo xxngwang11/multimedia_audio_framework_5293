79bb72b566c066220671fdc546b9ed557104ac2a
owenCreeper <ouhanjun3@huawei.com>
2025-07-17 20:44:44 +0800

    fix minus overflow
    Signed-off-by: owenCreeper <ouhanjun3@huawei.com>

--- services/audio_service/server/src/hpae_renderer_stream_impl.cpp
+++ services/audio_service/server/src/hpae_renderer_stream_impl.cpp
@@ -333,9 +333,10 @@ void HpaeRendererStreamImpl::GetLatencyInner(uint64_t &timestamp, uint64_t &late
     latencyUs += nearlinkLatency * AUDIO_US_PER_MS;
     std::vector<uint64_t> timestampCurrent = {0};
     ClockTime::GetAllTimeStamp(timestampCurrent);
-    auto interval = (timestampCurrent[baseUsed] - timestamp_[baseUsed]) / AUDIO_NS_PER_US;
-    interval = interval > latencyUs ? latencyUs : interval;
-    latencyUs -= interval;
+    auto interval = (timestampCurrent[baseUsed] > timestamp_[baseUsed]) ?
+        (timestampCurrent[baseUsed] - timestamp_[baseUsed]) / AUDIO_NS_PER_US :
+        0;
+    latencyUs = latencyUs > interval ? latencyUs - interval : 0;
     timestamp = timestampCurrent[baseUsed];
 
     AUDIO_DEBUG_LOG("Latency info: framePosition: %{public}" PRIu64 ", latencyUs %{public}" PRIu64
