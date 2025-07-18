xxxppp2025work/multimedia_audio_framework_gitcode

audio workgroup support qos7
Signed-off-by: fuxiuwsz <wangshengze1@huawei.com>

services/audio_service/client/src/audio_system_manager.cpp
@@ -37,6 +37,7 @@
 #include "rtg_interface.h"
 #include "istandard_audio_service.h"
 #include "audio_workgroup_callback_impl.h"
+#include "qos.h"
 using namespace OHOS::RME;
 
 namespace OHOS {
@@ -2120,6 +2121,10 @@ int32_t AudioSystemManager::CreateAudioWorkgroup()
     int32_t res = gasp->CreateAudioWorkgroup(getpid(), object, workgroupId);
     CHECK_AND_RETURN_RET_LOG(res == SUCCESS && workgroupId >= 0, AUDIO_ERR,
         "CreateAudioWorkgroup failed, res:%{public}d workgroupId:%{public}d", res, workgroupId);
+
+    if (res == SUCCESS) {
+        workgroupPrioRecorderMap.emplace(workgroupId, std::make_shared<WorkgroupPrioRecorder>(workgroupId));
+    }
     return workgroupId;
 }
 
@@ -2128,6 +2133,15 @@ int32_t AudioSystemManager::ReleaseAudioWorkgroup(int32_t workgroupId)
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
     int32_t ret = gasp->ReleaseAudioWorkgroup(getpid(), workgroupId);
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        if (recorder->RestoreGroupPrio(false) != AUDIO_OK) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] restore grp:%{public}d prio failed", workgroupId);
+        } else {
+            workgroupPrioRecorderMap.erase(workgroupId);
+        }
+    }
 
     int32_t pid = getpid();
     std::lock_guard<std::mutex> lock(startGroupPermissionMapMutex_);
@@ -2142,6 +2156,12 @@ int32_t AudioSystemManager::AddThreadToGroup(int32_t workgroupId, int32_t tokenI
 {
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        recorder->RecordThreadPrio(tokenId);
+    }
+
     return gasp->AddThreadToGroup(getpid(), workgroupId, tokenId);
 }
 
@@ -2149,19 +2169,51 @@ int32_t AudioSystemManager::RemoveThreadFromGroup(int32_t workgroupId, int32_t t
 {
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        if (recorder->RestoreThreadPrio(tokenId) != AUDIO_OK) {
+            AUDIO_INFO_LOG("[WorkgroupInClient] restore thread:%{public}d prio failed", tokenId);
+        }
+    }
+
     return gasp->RemoveThreadFromGroup(getpid(), workgroupId, tokenId);
 }
 
-int32_t AudioSystemManager::StartGroup(int32_t workgroupId, uint64_t startTime, uint64_t deadlineTime)
+int32_t AudioSystemManager::StartGroup(int32_t workgroupId, uint64_t startTime, uint64_t deadlineTime,
+    const std::unordered_map<int32_t, bool> threads, bool &needUpdatePrio)
 {
     if (!IsValidToStartGroup(workgroupId)) {
         return AUDIO_ERR;
     }
 
+    Trace trace("[WorkgroupInClient] StartGroup workgroupId:" + std::to_string(workgroupId) +
+        " startTime:" + std::to_string(startTime) + " deadlineTime:" + std::to_string(deadlineTime));
     CHECK_AND_RETURN_RET_LOG(deadlineTime > startTime, ERR_INVALID_PARAM, "Invalid Audio Deadline params");
-    SetFrameRateAndPrioType(workgroupId, MS_PER_SECOND/(deadlineTime - startTime), 0);
-    SetThreadQosLevel();
-    if (BeginFrameFreq(0) != 0) {
+    RME::SetFrameRateAndPrioType(workgroupId, MS_PER_SECOND/(deadlineTime - startTime), 0);
+
+    bool restoreByPermission = false;
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (!recorder) {
+        AUDIO_ERR_LOG("[WorkgroupInClient] GetRecorderByGrpId workgroupId:%{public}d failed", workgroupId);
+        return AUDIO_ERR;
+    }
+
+    restoreByPermission = recorder->GetRestoreByPermission();
+    if (needUpdatePrio || restoreByPermission) {
+        const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+        CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+        int32_t ipc_ret = gasp->ImproveAudioWorkgroupPrio(getpid(), threads);
+        if (ipc_ret != SUCCESS) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] change prio for grp:%{public}d failed, ret:%{public}d",
+                workgroupId, ipc_ret);
+            return AUDIO_ERR;
+        }
+        needUpdatePrio = false;
+        recorder->SetRestoreByPermission(false);
+    }
+
+    if (RME::BeginFrameFreq(deadlineTime - startTime) != 0) {
         AUDIO_ERR_LOG("[WorkgroupInClient] Audio Deadline BeginFrame failed");
         return AUDIO_ERR;
     }
@@ -2187,6 +2239,7 @@ void AudioSystemManager::OnWorkgroupChange(const AudioWorkgroupChangeInfo &info)
 {
     std::lock_guard<std::mutex> lock(startGroupPermissionMapMutex_);
     startGroupPermissionMap_[info.pid][info.groupId] = info.startAllowed;
+    std::vector<int32_t> workgroupIdNeedRestore;
 
     for (const auto &pair : startGroupPermissionMap_) {
         uint32_t pid = pair.first;
@@ -2196,6 +2249,18 @@ void AudioSystemManager::OnWorkgroupChange(const AudioWorkgroupChangeInfo &info)
             bool permissionValue = innerPair.second;
             AUDIO_INFO_LOG("[WorkgroupInClient] pid = %{public}d, groupId = %{public}d, startAllowed = %{public}d",
                 pid, grpId, permissionValue);
+            if (permissionValue == false) {
+                workgroupIdNeedRestore.push_back(grpId);
+            }
+        }
+    }
+
+    for (const auto &workgroupId : workgroupIdNeedRestore) {
+        std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+        if (recorder != nullptr) {
+            if (recorder->RestoreGroupPrio(true) != AUDIO_OK) {
+                AUDIO_INFO_LOG("[WorkgroupInClient] restore grp:%{public}d prio in cb failed", workgroupId);
+            }
         }
     }
 }
@@ -2225,6 +2290,84 @@ int32_t AudioSystemManager::ForceVolumeKeyControlType(AudioVolumeType volumeType
 {
     AUDIO_INFO_LOG("volumeType:%{public}d, duration:%{public}d", volumeType, duration);
     return AudioPolicyManager::GetInstance().ForceVolumeKeyControlType(volumeType, duration);
+}
+
+AudioSystemManager::WorkgroupPrioRecorder::WorkgroupPrioRecorder(int32_t grpId)
+{
+    grpId_ = grpId;
+    restoreByPermission_ = false;
+}
+ 
+void AudioSystemManager::WorkgroupPrioRecorder::SetRestoreByPermission(bool isByPermission)
+{
+    restoreByPermission_ = isByPermission;
+}
+ 
+bool AudioSystemManager::WorkgroupPrioRecorder::GetRestoreByPermission()
+{
+    return restoreByPermission_;
+}
+ 
+void AudioSystemManager::WorkgroupPrioRecorder::RecordThreadPrio(int32_t tokenId)
+{
+    auto it = threads_.find(tokenId);
+    if (it == threads_.end()) {
+        OHOS::QOS::QosLevel qosLevel;
+        int32_t ret = OHOS::QOS::GetThreadQos(qosLevel);
+        threads_[tokenId] = (ret == -1 ? -1 : (int32_t)qosLevel);
+    }
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::RestoreGroupPrio(bool isByPermission)
+{
+    const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+    CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    if (gasp->RestoreAudioWorkgroupPrio(getpid(), threads_) != AUDIO_OK) {
+        AUDIO_ERR_LOG("[WorkgroupInClient] restore prio for workgroupId:%{public}d failed", GetGrpId());
+        return AUDIO_ERR;
+    }
+
+    if (!isByPermission) {
+        threads_.clear();
+    } else {
+        restoreByPermission_ = true;
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::RestoreThreadPrio(int32_t tokenId)
+{
+    const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+    CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+    auto it = threads_.find(tokenId);
+    int ipc_ret;
+    if (it != threads_.end()) {
+        std::unordered_map<int32_t, int32_t> thread = {{it->first, it->second}};
+        ipc_ret = gasp->RestoreAudioWorkgroupPrio(getpid(), thread);
+        if (ipc_ret != SUCCESS) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] change prio for tokenId:%{public}d failed, ret:%{public}d",
+                tokenId, ipc_ret);
+            return AUDIO_ERR;
+        } else {
+            threads_.erase(tokenId);
+        }
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::GetGrpId()
+{
+    return grpId_;
+}
+ 
+std::shared_ptr<AudioSystemManager::WorkgroupPrioRecorder> AudioSystemManager::GetRecorderByGrpId(int32_t grpId)
+{
+    auto it = workgroupPrioRecorderMap.find(grpId);
+    if (it != workgroupPrioRecorderMap.end()) {
+        return it->second;
+    }
+    return nullptr;
 }
 } // namespace AudioStandard
 } // namespace OHOS

services/audio_service/server/src/audio_resource_service.cpp
@@ -24,6 +24,7 @@
 #include "audio_utils.h"
 #include "audio_workgroup_callback_proxy.h"
 #include "audio_workgroup_callback.h"
+#include "audio_schedule.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -368,6 +369,27 @@ std::vector<int32_t> AudioResourceService::GetProcessesOfAudioWorkgroup()
         keys.push_back(pair.first);
     }
     return keys;
+}
+
+int32_t AudioResourceService::ImproveAudioWorkgroupPrio(int32_t pid,
+    const std::unordered_map<int32_t, bool> &threads)
+{
+    for (const auto &tid : threads) {
+        AUDIO_ERR_LOG("[WorkgroupInServer]set pid:%{public}d tid:%{public}d to qos_level7", pid, tid.first);
+        ScheduleReportData(pid, tid.first, "audio_server");
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioResourceService::RestoreAudioWorkgroupPrio(int32_t pid,
+    const std::unordered_map<int32_t, int32_t> &threads)
+{
+    for (const auto &tid : threads) {
+        AUDIO_ERR_LOG("[WorkgroupInServer]set pid:%{public}d tid:%{public}d to qos%{public}d",
+            pid, tid.first, tid.second);
+        ScheduleReportDataWithQosLevel(pid, tid.first, "audio_server", tid.second);
+    }
+    return AUDIO_OK;
 }
 } // namespce AudioStandard
 } // namespace OHOS

xxngwang11/multimedia_audio_framework_5293

services/audio_service/client/src/audio_system_manager.cpp
@@ -35,6 +35,7 @@
 #include "audio_workgroup_callback_stub.h"
 #include "audio_qosmanager.h"
 #include "rtg_interface.h"
+#include "qos.h"
 #include "istandard_audio_service.h"
 #include "audio_workgroup_callback_impl.h"
 using namespace OHOS::RME;
@@ -2120,6 +2121,9 @@ int32_t AudioSystemManager::CreateAudioWorkgroup()
     int32_t res = gasp->CreateAudioWorkgroup(getpid(), object, workgroupId);
     CHECK_AND_RETURN_RET_LOG(res == SUCCESS && workgroupId >= 0, AUDIO_ERR,
         "CreateAudioWorkgroup failed, res:%{public}d workgroupId:%{public}d", res, workgroupId);
+
+    std::lock_guard<std::mutex> lock(workgroupPrioRecorderMutex_);
+    workgroupPrioRecorderMap_.emplace(workgroupId, std::make_shared<WorkgroupPrioRecorder>(workgroupId));
     return workgroupId;
 }
 
@@ -2128,6 +2132,16 @@ int32_t AudioSystemManager::ReleaseAudioWorkgroup(int32_t workgroupId)
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
     int32_t ret = gasp->ReleaseAudioWorkgroup(getpid(), workgroupId);
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        if (recorder->RestoreGroupPrio(false) != AUDIO_OK) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] restore grp:%{public}d prio failed", workgroupId);
+        } else {
+            std::lock_guard<std::mutex> lock(workgroupPrioRecorderMutex_);
+            workgroupPrioRecorderMap_.erase(workgroupId);
+        }
+    }
 
     int32_t pid = getpid();
     std::lock_guard<std::mutex> lock(startGroupPermissionMapMutex_);
@@ -2142,6 +2156,12 @@ int32_t AudioSystemManager::AddThreadToGroup(int32_t workgroupId, int32_t tokenI
 {
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        recorder->RecordThreadPrio(tokenId);
+    }
+
     return gasp->AddThreadToGroup(getpid(), workgroupId, tokenId);
 }
 
@@ -2149,19 +2169,62 @@ int32_t AudioSystemManager::RemoveThreadFromGroup(int32_t workgroupId, int32_t t
 {
     const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
     CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (recorder != nullptr) {
+        if (recorder->RestoreThreadPrio(tokenId) != AUDIO_OK) {
+            AUDIO_INFO_LOG("[WorkgroupInClient] restore thread:%{public}d prio failed", tokenId);
+        }
+    }
+
     return gasp->RemoveThreadFromGroup(getpid(), workgroupId, tokenId);
 }
 
-int32_t AudioSystemManager::StartGroup(int32_t workgroupId, uint64_t startTime, uint64_t deadlineTime)
+int32_t AudioSystemManager::ExcuteAudioWorkgroupPrioImprove(int32_t workgroupId,
+    const std::unordered_map<int32_t, bool> threads, bool &needUpdatePrio)
+{
+    bool restoreByPermission = false;
+    std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+    if (!recorder) {
+        AUDIO_ERR_LOG("[WorkgroupInClient] GetRecorderByGrpId workgroupId:%{public}d failed", workgroupId);
+        return AUDIO_ERR;
+    }
+
+    restoreByPermission = recorder->GetRestoreByPermission();
+    if (needUpdatePrio || restoreByPermission) {
+        const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+        CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+        int32_t ipcRet = gasp->ImproveAudioWorkgroupPrio(getpid(), threads);
+        if (ipcRet != SUCCESS) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] change prio for grp:%{public}d failed, ret:%{public}d",
+                workgroupId, ipcRet);
+            return AUDIO_ERR;
+        }
+        needUpdatePrio = false;
+        recorder->SetRestoreByPermission(false);
+    }
+
+    return AUDIO_OK;
+}
+
+int32_t AudioSystemManager::StartGroup(int32_t workgroupId, uint64_t startTime, uint64_t deadlineTime,
+    const std::unordered_map<int32_t, bool> threads, bool &needUpdatePrio)
 {
     if (!IsValidToStartGroup(workgroupId)) {
         return AUDIO_ERR;
     }
 
+    Trace trace("[WorkgroupInClient] StartGroup workgroupId:" + std::to_string(workgroupId) +
+        " startTime:" + std::to_string(startTime) + " deadlineTime:" + std::to_string(deadlineTime));
     CHECK_AND_RETURN_RET_LOG(deadlineTime > startTime, ERR_INVALID_PARAM, "Invalid Audio Deadline params");
-    SetFrameRateAndPrioType(workgroupId, MS_PER_SECOND/(deadlineTime - startTime), 0);
-    SetThreadQosLevel();
-    if (BeginFrameFreq(0) != 0) {
+    RME::SetFrameRateAndPrioType(workgroupId, MS_PER_SECOND/(deadlineTime - startTime), 0);
+
+    if (ExcuteAudioWorkgroupPrioImprove(workgroupId, threads, needUpdatePrio) != AUDIO_OK) {
+        AUDIO_ERR_LOG("[WorkgroupInClient] excute audioworkgroup prio improve failed");
+        return AUDIO_ERR;
+    }
+
+    if (RME::BeginFrameFreq(deadlineTime - startTime) != 0) {
         AUDIO_ERR_LOG("[WorkgroupInClient] Audio Deadline BeginFrame failed");
         return AUDIO_ERR;
     }
@@ -2187,6 +2250,7 @@ void AudioSystemManager::OnWorkgroupChange(const AudioWorkgroupChangeInfo &info)
 {
     std::lock_guard<std::mutex> lock(startGroupPermissionMapMutex_);
     startGroupPermissionMap_[info.pid][info.groupId] = info.startAllowed;
+    std::vector<int32_t> workgroupIdNeedRestore;
 
     for (const auto &pair : startGroupPermissionMap_) {
         uint32_t pid = pair.first;
@@ -2196,6 +2260,18 @@ void AudioSystemManager::OnWorkgroupChange(const AudioWorkgroupChangeInfo &info)
             bool permissionValue = innerPair.second;
             AUDIO_INFO_LOG("[WorkgroupInClient] pid = %{public}d, groupId = %{public}d, startAllowed = %{public}d",
                 pid, grpId, permissionValue);
+            if (permissionValue == false) {
+                workgroupIdNeedRestore.push_back(grpId);
+            }
+        }
+    }
+
+    for (const auto &workgroupId : workgroupIdNeedRestore) {
+        std::shared_ptr<WorkgroupPrioRecorder> recorder = GetRecorderByGrpId(workgroupId);
+        if (recorder != nullptr) {
+            if (recorder->RestoreGroupPrio(true) != AUDIO_OK) {
+                AUDIO_INFO_LOG("[WorkgroupInClient] restore grp:%{public}d prio in cb failed", workgroupId);
+            }
         }
     }
 }
@@ -2225,6 +2301,88 @@ int32_t AudioSystemManager::ForceVolumeKeyControlType(AudioVolumeType volumeType
 {
     AUDIO_INFO_LOG("volumeType:%{public}d, duration:%{public}d", volumeType, duration);
     return AudioPolicyManager::GetInstance().ForceVolumeKeyControlType(volumeType, duration);
+}
+
+AudioSystemManager::WorkgroupPrioRecorder::WorkgroupPrioRecorder(int32_t grpId)
+{
+    grpId_ = grpId;
+    restoreByPermission_ = false;
+}
+ 
+void AudioSystemManager::WorkgroupPrioRecorder::SetRestoreByPermission(bool isByPermission)
+{
+    restoreByPermission_ = isByPermission;
+}
+ 
+bool AudioSystemManager::WorkgroupPrioRecorder::GetRestoreByPermission()
+{
+    return restoreByPermission_;
+}
+ 
+void AudioSystemManager::WorkgroupPrioRecorder::RecordThreadPrio(int32_t tokenId)
+{
+    std::lock_guard<std::mutex> lock(workgroupThreadsMutex_);
+    auto it = threads_.find(tokenId);
+    if (it == threads_.end()) {
+        OHOS::QOS::QosLevel qosLevel;
+        int32_t ret = OHOS::QOS::GetThreadQos(qosLevel);
+        threads_[tokenId] = (ret == -1 ? -1 : (int32_t)qosLevel);
+    }
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::RestoreGroupPrio(bool isByPermission)
+{
+    const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+    CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+
+    std::lock_guard<std::mutex> lock(workgroupThreadsMutex_);
+    if (gasp->RestoreAudioWorkgroupPrio(getpid(), threads_) != AUDIO_OK) {
+        AUDIO_ERR_LOG("[WorkgroupInClient] restore prio for workgroupId:%{public}d failed", GetGrpId());
+        return AUDIO_ERR;
+    }
+
+    if (!isByPermission) {
+        threads_.clear();
+    } else {
+        restoreByPermission_ = true;
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::RestoreThreadPrio(int32_t tokenId)
+{
+    const sptr<IStandardAudioService> gasp = GetAudioSystemManagerProxy();
+    CHECK_AND_RETURN_RET_LOG(gasp != nullptr, ERR_INVALID_PARAM, "Audio service unavailable.");
+    std::lock_guard<std::mutex> lock(workgroupThreadsMutex_);
+    auto it = threads_.find(tokenId);
+    int ipcRet;
+    if (it != threads_.end()) {
+        std::unordered_map<int32_t, int32_t> thread = {{it->first, it->second}};
+        ipcRet = gasp->RestoreAudioWorkgroupPrio(getpid(), thread);
+        if (ipcRet != SUCCESS) {
+            AUDIO_ERR_LOG("[WorkgroupInClient] change prio for tokenId:%{public}d failed, ret:%{public}d",
+                tokenId, ipcRet);
+            return AUDIO_ERR;
+        } else {
+            threads_.erase(tokenId);
+        }
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioSystemManager::WorkgroupPrioRecorder::GetGrpId()
+{
+    return grpId_;
+}
+ 
+std::shared_ptr<AudioSystemManager::WorkgroupPrioRecorder> AudioSystemManager::GetRecorderByGrpId(int32_t grpId)
+{
+    std::lock_guard<std::mutex> lock(workgroupPrioRecorderMutex_);
+    auto it = workgroupPrioRecorderMap_.find(grpId);
+    if (it != workgroupPrioRecorderMap_.end()) {
+        return it->second;
+    }
+    return nullptr;
 }
 } // namespace AudioStandard
 } // namespace OHOS

services/audio_service/server/src/audio_resource_service.cpp
@@ -24,6 +24,7 @@
 #include "audio_utils.h"
 #include "audio_workgroup_callback_proxy.h"
 #include "audio_workgroup_callback.h"
+#include "audio_schedule.h"
 
 namespace OHOS {
 namespace AudioStandard {
@@ -368,6 +369,27 @@ std::vector<int32_t> AudioResourceService::GetProcessesOfAudioWorkgroup()
         keys.push_back(pair.first);
     }
     return keys;
+}
+
+int32_t AudioResourceService::ImproveAudioWorkgroupPrio(int32_t pid,
+    const std::unordered_map<int32_t, bool> &threads)
+{
+    for (const auto &tid : threads) {
+        AUDIO_INFO_LOG("[WorkgroupInServer]set pid:%{public}d tid:%{public}d to qos_level7", pid, tid.first);
+        ScheduleReportData(pid, tid.first, "audio_server");
+    }
+    return AUDIO_OK;
+}
+ 
+int32_t AudioResourceService::RestoreAudioWorkgroupPrio(int32_t pid,
+    const std::unordered_map<int32_t, int32_t> &threads)
+{
+    for (const auto &tid : threads) {
+        AUDIO_INFO_LOG("[WorkgroupInServer]set pid:%{public}d tid:%{public}d to qos%{public}d",
+            pid, tid.first, tid.second);
+        ScheduleReportDataWithQosLevel(pid, tid.first, "audio_server", tid.second);
+    }
+    return AUDIO_OK;
 }
 } // namespce AudioStandard
 } // namespace OHOS