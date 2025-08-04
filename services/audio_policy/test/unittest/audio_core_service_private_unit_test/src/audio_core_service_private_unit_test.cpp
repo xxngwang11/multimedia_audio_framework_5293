5f3669a1559ee67e54ce6f71ee45acb7d3293a2a
ruiyangsun <sunruiyang5@h-partners.com>
2025-08-04 17:00:49 +0800

    add ActivateInputDevice tdd
    Signed-off-by: ruiyangsun <sunruiyang5@h-partners.com>

--- services/audio_policy/test/unittest/audio_core_service_private_unit_test/src/audio_core_service_private_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_core_service_private_unit_test/src/audio_core_service_private_unit_test.cpp
@@ -2651,11 +2651,32 @@ HWTEST(AudioCoreServicePrivateTest, ActivateInputDevice_001, TestSize.Level1)
 
     auto result = audioCoreService->ActivateInputDevice(streamDesc);
     ASSERT_EQ(result, SUCCESS);
-    
-    deviceDesc->deviceType_ = DeviceType::DEVICE_TYPE_NONE;
-    result = audioCoreService->ActivateInputDevice(streamDesc);
-    ASSERT_EQ(result, SUCCESS);
     AUDIO_INFO_LOG("AudioCoreServicePrivateTest ActivateInputDevice_001 end");
+}
+
+/**
+ * @tc.name  : Test AudioCoreService.
+ * @tc.number: ActivateInputDevice_002
+ * @tc.desc  : Test AudioCoreService::ActivateInputDevice()
+ */
+HWTEST(AudioCoreServicePrivateTest, ActivateInputDevice_002, TestSize.Level1)
+{
+    AUDIO_INFO_LOG("AudioCoreServicePrivateTest ActivateInputDevice_002 start");
+    auto audioCoreService = std::make_shared<AudioCoreService>();
+    ASSERT_NE(audioCoreService, nullptr);
+
+    std::shared_ptr<AudioStreamDescriptor> streamDesc = std::make_shared<AudioStreamDescriptor>();
+    ASSERT_NE(streamDesc, nullptr);
+
+    auto deviceDesc = std::make_shared<AudioDeviceDescriptor>(
+        DeviceType::DEVICE_TYPE_NONE,
+        DeviceRole::INPUT_DEVICE);
+    ASSERT_NE(deviceDesc, nullptr);
+    streamDesc->newDeviceDescs_.push_back(deviceDesc);
+
+    auto result = audioCoreService->ActivateInputDevice(streamDesc);
+    ASSERT_EQ(result, SUCCESS);
+    AUDIO_INFO_LOG("AudioCoreServicePrivateTest ActivateInputDevice_002 end");
 }
 
 /**
