d43c191e1876e1da7eadc0df6f062616f380e239
wangchongteng <wangchongteng@huawei.com>
2025-08-06 15:27:08 +0800

    修改ProAudio数据处理线程优先级
    Signed-off-by: wangchongteng <wangchongteng@huawei.com>

--- frameworks/native/audioqosmanager/test/unittest/audio_qosmanager_unit_test.cpp
+++ frameworks/native/audioqosmanager/test/unittest/audio_qosmanager_unit_test.cpp
@@ -15,9 +15,16 @@
 
 #include "audio_qosmanager.h"
 #include <gtest/gtest.h>
+#include <unistd.h>
 
 using namespace testing::ext;
 using namespace std;
+
+namespace {
+static constexpr int32_t WAIT_FOR_SET_QOS_TIME_US = 500000; // 500ms
+static constexpr int32_t SET_PRIORITY_1 = 1;
+static constexpr int32_t SET_PRIORITY_4 = 4;
+}
 
 class AudioQosmanagerUnitTest : public ::testing::Test {
 public:
@@ -34,6 +41,12 @@ public:
  */
 HWTEST_F(AudioQosmanagerUnitTest, SetThreadQosLevelAsync_001, TestSize.Level4)
 {
-    SetThreadQosLevelAsync();
+    SetThreadQosLevelAsync(SET_PRIORITY_1);
+    usleep(WAIT_FOR_SET_QOS_TIME_US);
+    ResetThreadQosLevel();
+    EXPECT_TRUE(gettid());
+    SetThreadQosLevelAsync(SET_PRIORITY_4);
+    usleep(WAIT_FOR_SET_QOS_TIME_US);
+    ResetThreadQosLevel();
     EXPECT_TRUE(gettid());
 }
