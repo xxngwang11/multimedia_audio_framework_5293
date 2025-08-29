626b9909d957d8f250903c4cb2cbd66f9d280c85
陈耀文 <chenyaowen6@huawei.com>
2025-08-29 14:35:18 +0800

    清除通话强选需求开发
    Signed-off-by: 陈耀文 <chenyaowen6@huawei.com>

--- services/audio_policy/server/domain/device/src/audio_device_common.cpp
+++ services/audio_policy/server/domain/device/src/audio_device_common.cpp
@@ -156,9 +156,10 @@ void AudioDeviceCommon::OnPreferredInputDeviceUpdated(DeviceType deviceType, std
 }
 
 std::vector<std::shared_ptr<AudioDeviceDescriptor>> AudioDeviceCommon::GetPreferredOutputDeviceDescInner(
-    AudioRendererInfo &rendererInfo, std::string networkId)
+    AudioRendererInfo &rendererInfo, std::string networkId, const int32_t uid)
 {
     std::vector<std::shared_ptr<AudioDeviceDescriptor>> deviceList = {};
+    RouterType bypassType = RouterType::ROUTER_TYPE_NONE;
     if (rendererInfo.streamUsage <= STREAM_USAGE_UNKNOWN ||
         rendererInfo.streamUsage > STREAM_USAGE_MAX) {
         AUDIO_WARNING_LOG("Invalid usage[%{public}d], return current device.", rendererInfo.streamUsage);
@@ -168,6 +169,19 @@ std::vector<std::shared_ptr<AudioDeviceDescriptor>> AudioDeviceCommon::GetPrefer
         return deviceList;
     }
     if (networkId == LOCAL_NETWORK_ID) {
+        auto preferredType = AudioPolicyUtils::GetInstance().GetPreferredTypeByStreamUsage(rendererInfo.streamUsage);
+        if (preferredType == AUDIO_CALL_RENDER) {
+            if (uid >= 0) {
+                bypassType = RouterType::ROUTER_TYPE_USER_SELECT;
+                shared_ptr<AudioDeviceDescriptor> preferredDevice = 
+                    AudioStateManager::GetAudioStateManager().GetPreferredCallRenderDeviceForUid(uid);
+                CHECK_AND_RETURN_RET_LOG(preferredDevice != nullptr, deviceList, "preferredDevice is nullptr.");
+                if (preferredDevice->deviceId_ != 0) {
+                    deviceList.push_back(preferredDevice);
+                    return deviceList;
+                }
+            } 
+        }
         vector<std::shared_ptr<AudioDeviceDescriptor>> descs =
             audioRouterCenter_.FetchOutputDevices(rendererInfo.streamUsage,
                 -1, "GetPreferredOutputDeviceDescInner");
