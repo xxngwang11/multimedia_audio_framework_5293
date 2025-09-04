fef87136bec881eedcd7670a9a3aea16ed98da25
蒋志庆 <jiangzhiqing4@h-partners.com>
2025-09-04 11:36:34 +0800

    商用日志整改
    Signed-off-by: 蒋志庆 <jiangzhiqing4@h-partners.com>

--- frameworks/native/audioutils/src/audio_utils.cpp
+++ frameworks/native/audioutils/src/audio_utils.cpp
@@ -1194,11 +1194,10 @@ int32_t GetEngineFlag()
     static int32_t engineFlag = -1;
     if (engineFlag == -1) {
         bool res = GetSysPara(para.c_str(), engineFlag);
-        AUDIO_DEBUG_LOG("get %{public}s = %{public}d", para.c_str(), engineFlag);
+        AUDIO_INFO_LOG("get %{public}s = %{public}d", para.c_str(), engineFlag);
         CHECK_AND_RETURN_RET_LOG(res, engineFlag, "get %{public}s fail", para.c_str());
     }
 #else
-    AUDIO_DEBUG_LOG("old engine is not supported");
     return 1;
 #endif
 }
