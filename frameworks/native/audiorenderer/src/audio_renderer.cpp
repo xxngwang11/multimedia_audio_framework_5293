985af0e7bcebe6b717ccd4c5de6cddf02c471a42
ruiyangsun <sunruiyang5@h-partners.com>
2025-09-05 15:22:03 +0800

    use correct streamClass
    Signed-off-by: ruiyangsun <sunruiyang5@h-partners.com>

--- frameworks/native/audiorenderer/src/audio_renderer.cpp
+++ frameworks/native/audiorenderer/src/audio_renderer.cpp
@@ -2269,7 +2269,7 @@ bool AudioRendererPrivate::GenerateNewStream(IAudioStream::StreamClass targetCla
     bool switchResult = false;
     std::shared_ptr<IAudioStream> oldAudioStream = nullptr;
     // create new IAudioStream
-    SetClientInfo(flag, streamClass);
+    SetClientInfo(restoreInfo.routeFlag, targetClass);
     std::shared_ptr<IAudioStream> newAudioStream = IAudioStream::GetPlaybackStream(targetClass, switchInfo.params,
         switchInfo.eStreamType, appInfo_.appUid);
     CHECK_AND_RETURN_RET_LOG(newAudioStream != nullptr, false, "SetParams GetPlayBackStream failed.");
