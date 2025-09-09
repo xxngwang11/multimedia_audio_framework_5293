0f84f48a803bfbf961c8642d2a2e0e82bb983844
owenCreeper <ouhanjun3@huawei.com>
2025-09-09 17:38:36 +0800

    fix conflict
    Signed-off-by: owenCreeper <ouhanjun3@huawei.com>

--- frameworks/native/offlineaudioeffect/include/offline_audio_effect_server_chain.h
+++ frameworks/native/offlineaudioeffect/include/offline_audio_effect_server_chain.h
@@ -72,7 +72,6 @@ private:
     uint32_t inBufferSize_ = 0;
     uint32_t outBufferSize_ = 0;
     string chainName_;
-    mutex offlineChainMutex_;
     OfflineEffectIOConfig offlineConfig_ = {};
     bool firstProcess_ = false;
     FILE *dumpFileIn_ = nullptr;
