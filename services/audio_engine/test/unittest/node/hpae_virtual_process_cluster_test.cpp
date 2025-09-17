bcc88042a9889b7d27b06e4a123de3771e25843f
Charles <wangchao685@h-partners.com>
2025-09-17 19:06:04 +0800

    proaudio inject
    Signed-off-by: Charles <wangchao685@h-partners.com>

--- services/audio_engine/test/unittest/node/hpae_virtual_process_cluster_test.cpp
+++ services/audio_engine/test/unittest/node/hpae_virtual_process_cluster_test.cpp
@@ -13,7 +13,6 @@
  * limitations under the License.
  */
 
-
 #include <gtest/gtest.h>
 #include "audio_errors.h"
 #include "hpae_virtual_process_cluster.h"
@@ -21,6 +20,7 @@
 #include "hpae_gain_node.h"
 #include "hpae_sink_input_node.h"
 #include "hpae_audio_format_converter_node.h"
+#include "hpae_node_common.h"
 using namespace testing::ext;
 using namespace testing;
 
@@ -30,24 +30,28 @@ namespace HPAE {
 static constexpr uint32_t DEFAULT_SESSION_ID01 = 100000;
 static constexpr uint32_t DEFAULT_SESSION_ID02 = 100001;
 class HpaeVirtualProcessClusterTest : public testing::Test {
-protected:
-    void SetUp() override {
-        nodeInfo_.nodeId = 1;
-        nodeInfo_.samplingRate = SAMPLE_RATE_48000;
-        nodeInfo_.frameLen = 960;
-        nodeInfo_.channels = STEREO;
-        nodeInfo_.format = SAMPLE_F32LE;
-        nodeInfo_.sessionId = DEFAULT_SESSION_ID01;
-    }
+public:
+    void SetUp() override {}
 
     void TearDown() override {}
-
-    HpaeNodeInfo nodeInfo_;
 };
+
+static HpaeNodeInfo GetNodeInfo()
+{
+    HpaeNodeInfo nodeInfo;
+    nodeInfo.nodeId = 1;
+    nodeInfo.samplingRate = SAMPLE_RATE_48000;
+    nodeInfo.frameLen = 960; // 960 for framelen
+    nodeInfo.channels = STEREO;
+    nodeInfo.format = SAMPLE_F32LE;
+    nodeInfo.sessionId = DEFAULT_SESSION_ID01;
+    return nodeInfo;
+}
 
 HWTEST_F(HpaeVirtualProcessClusterTest, ConstructHpaeVirtualProcessCluster, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
     EXPECT_NE(cluster->GetSharedInstance(), nullptr);
     std::shared_ptr<HpaeNode> mixerInstance = cluster->GetSharedInstance();
@@ -58,10 +62,12 @@ HWTEST_F(HpaeVirtualProcessClusterTest, ConstructHpaeVirtualProcessCluster, Test
 
 HWTEST_F(HpaeVirtualProcessClusterTest, DestructorCleansUpResources, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    HpaeNodeInfo nodeInfo = nodeInfo_;
-    nodeInfo.samplingRate = SAMPLE_RATE_44100;
-    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(nodeInfo);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    HpaeNodeInfo nodeInfo1 = nodeInfo;
+    nodeInfo1.samplingRate = SAMPLE_RATE_44100;
+    nodeInfo1.frameLen = CaculateFrameLenByNodeInfo(nodeInfo1);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(nodeInfo1);
     cluster->Connect(preNode);
     EXPECT_EQ(cluster->GetConnectSinkInputNum(), 1);
     
@@ -69,31 +75,34 @@ HWTEST_F(HpaeVirtualProcessClusterTest, DestructorCleansUpResources, TestSize.Le
 
 HWTEST_F(HpaeVirtualProcessClusterTest, DoProcessDelegatesToMixerNode, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     EXPECT_NO_FATAL_FAILURE(cluster->DoProcess());
 }
 
 HWTEST_F(HpaeVirtualProcessClusterTest, ResetClearsMixerNode, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    auto preNode = std::make_shared<HpaeSinkInputNode>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(nodeInfo);
     cluster->Connect(preNode);
-    EXPECT_GT(cluster->GetConnectSinkInputNum(), 1);
+    EXPECT_EQ(preNode.use_count(), 2);
     EXPECT_EQ(cluster->Reset(), true);
-    EXPECT_EQ(cluster->GetConnectSinkInputNum(), 0);
+    EXPECT_EQ(preNode.use_count(), 2);
 }
 
 HWTEST_F(HpaeVirtualProcessClusterTest, ResetAllClearsAllConnections, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
-    HpaeNodeInfo info1 = nodeInfo_;
+    HpaeNodeInfo info1 = GetNodeInfo();;
     info1.sessionId = DEFAULT_SESSION_ID01;
-    HpaeNodeInfo info2 = nodeInfo_;
+    HpaeNodeInfo info2 = GetNodeInfo();;
     info2.sessionId = DEFAULT_SESSION_ID02;
     
-    auto preNode1 = std::make_shared<HpaeSinkInputNode>(info1);
-    auto preNode2 = std::make_shared<HpaeSinkInputNode>(info2);
+    std::shared_ptr<HpaeSinkInputNode> preNode1 = std::make_shared<HpaeSinkInputNode>(info1);
+    std::shared_ptr<HpaeSinkInputNode> preNode2 = std::make_shared<HpaeSinkInputNode>(info2);
     cluster->Connect(preNode1);
     cluster->Connect(preNode2);
     EXPECT_EQ(cluster->GetConnectSinkInputNum(), 2);
@@ -104,23 +113,26 @@ HWTEST_F(HpaeVirtualProcessClusterTest, ResetAllClearsAllConnections, TestSize.L
 
 HWTEST_F(HpaeVirtualProcessClusterTest, GetSharedInstanceReturnsMixerNode, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    EXPECT_NE(cluster->GetSharedInstance()!= nullptr, true);
-    EXPECT_EQ(cluster->GetSharedInstance()->GetNodeId(), nodeInfo_.nodeId);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    EXPECT_EQ(cluster->GetSharedInstance()!= nullptr, true);
+    EXPECT_EQ(cluster->GetSharedInstance()->GetChannelCount(), nodeInfo.channels);
 }
 
 HWTEST_F(HpaeVirtualProcessClusterTest, GetOutputPortReturnsMixerPort, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    EXPECT_NE(cluster->GetOutputPort() != nullptr, true);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    EXPECT_EQ(cluster->GetOutputPort() != nullptr, true);
 }
 
 HWTEST_F(HpaeVirtualProcessClusterTest, ConnectCreatesGainAndConverterNodes, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     preNodeInfo.sessionId = DEFAULT_SESSION_ID01;
-    auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
     cluster->Connect(preNode);
     std::shared_ptr<HpaeGainNode> gainNode = cluster->GetGainNodeById(preNodeInfo.sessionId);
     EXPECT_NE(gainNode, nullptr);
@@ -129,10 +141,11 @@ HWTEST_F(HpaeVirtualProcessClusterTest, ConnectCreatesGainAndConverterNodes, Tes
 
 HWTEST_F(HpaeVirtualProcessClusterTest, ConnectSameSessionIdNoDuplicates, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     preNodeInfo.sessionId = DEFAULT_SESSION_ID01;
-    auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
     cluster->Connect(preNode);
     size_t initialCount = cluster->GetConnectSinkInputNum();
     cluster->Connect(preNode);
@@ -141,11 +154,12 @@ HWTEST_F(HpaeVirtualProcessClusterTest, ConnectSameSessionIdNoDuplicates, TestSi
 
 HWTEST_F(HpaeVirtualProcessClusterTest, DisConnectRemovesNodesFromMaps, TestSize.Level0)
 {
-   std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+   HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     preNodeInfo.sessionId = DEFAULT_SESSION_ID01;
-    auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
     cluster->Connect(preNode);
     EXPECT_EQ(cluster->GetConnectSinkInputNum(), 1);
     std::shared_ptr<HpaeGainNode> gainNodeBefore = cluster->GetGainNodeById(preNodeInfo.sessionId);
@@ -158,17 +172,18 @@ HWTEST_F(HpaeVirtualProcessClusterTest, DisConnectRemovesNodesFromMaps, TestSize
 
 HWTEST_F(HpaeVirtualProcessClusterTest, DisConnectNonExistentSession, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     preNodeInfo.sessionId = DEFAULT_SESSION_ID01;
-    auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
-    
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
     EXPECT_NO_FATAL_FAILURE(cluster->DisConnect(preNode));
 }
 
 HWTEST_F(HpaeVirtualProcessClusterTest, SetupAudioLimiterReturnsSuccess, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
     int32_t result = cluster->SetupAudioLimiter();
     EXPECT_EQ(result, SUCCESS);
@@ -176,17 +191,18 @@ HWTEST_F(HpaeVirtualProcessClusterTest, SetupAudioLimiterReturnsSuccess, TestSiz
 
 HWTEST_F(HpaeVirtualProcessClusterTest, GetConnectSinkInputNumReturnsCorrectCount, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
     EXPECT_EQ(cluster->GetConnectSinkInputNum(), 0);
     
-    HpaeNodeInfo info1 = nodeInfo_;
+    HpaeNodeInfo info1 = GetNodeInfo();
     info1.sessionId = DEFAULT_SESSION_ID01;
-    HpaeNodeInfo info2 = nodeInfo_;
+    HpaeNodeInfo info2 = GetNodeInfo();
     info2.sessionId = DEFAULT_SESSION_ID02;
     
-    auto preNode1 = std::make_shared<HpaeSinkInputNode>(info1);
-    auto preNode2 = std::make_shared<HpaeSinkInputNode>(info2);
+    std::shared_ptr<HpaeSinkInputNode> preNode1 = std::make_shared<HpaeSinkInputNode>(info1);
+    std::shared_ptr<HpaeSinkInputNode> preNode2 = std::make_shared<HpaeSinkInputNode>(info2);
     
     cluster->Connect(preNode1);
     EXPECT_EQ(cluster->GetConnectSinkInputNum(), 1);
@@ -203,11 +219,12 @@ HWTEST_F(HpaeVirtualProcessClusterTest, GetConnectSinkInputNumReturnsCorrectCoun
 
 HWTEST_F(HpaeVirtualProcessClusterTest, GetGainNodeByIdReturnsCorrectNode, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     preNodeInfo.sessionId = DEFAULT_SESSION_ID01;
-    auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+    std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
     
     cluster->Connect(preNode);
     
@@ -218,7 +235,8 @@ HWTEST_F(HpaeVirtualProcessClusterTest, GetGainNodeByIdReturnsCorrectNode, TestS
 
 HWTEST_F(HpaeVirtualProcessClusterTest, GetGainNodeByIdNonExistentReturnsNull, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     
     std::shared_ptr<HpaeGainNode> gainNode = cluster->GetGainNodeById(9999);
     EXPECT_EQ(gainNode, nullptr);
@@ -226,13 +244,14 @@ HWTEST_F(HpaeVirtualProcessClusterTest, GetGainNodeByIdNonExistentReturnsNull, T
 
 HWTEST_F(HpaeVirtualProcessClusterTest, MultipleConnectDisconnectOperations, TestSize.Level0)
 {
-    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo_);
+    HpaeNodeInfo nodeInfo = GetNodeInfo();
+    std::shared_ptr<HpaeVirtualProcessCluster> cluster = std::make_shared<HpaeVirtualProcessCluster>(nodeInfo);
     const int32_t numOperations = 5;
-    HpaeNodeInfo preNodeInfo = nodeInfo_;
+    HpaeNodeInfo preNodeInfo = GetNodeInfo();
     
     for (int32_t i = 0; i < numOperations; i++) {
         preNodeInfo.sessionId = DEFAULT_SESSION_ID01 + i;
-        auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+        std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
         cluster->Connect(preNode);
         EXPECT_EQ(cluster->GetConnectSinkInputNum(), i + 1);
         std::shared_ptr<HpaeGainNode> gainNode = cluster->GetGainNodeById(preNodeInfo.sessionId);
@@ -241,7 +260,7 @@ HWTEST_F(HpaeVirtualProcessClusterTest, MultipleConnectDisconnectOperations, Tes
     
     for (int32_t i = 0; i < numOperations; i++) {
         preNodeInfo.sessionId = DEFAULT_SESSION_ID01 + i;
-        auto preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
+        std::shared_ptr<HpaeSinkInputNode> preNode = std::make_shared<HpaeSinkInputNode>(preNodeInfo);
         
         cluster->DisConnect(preNode);
         EXPECT_EQ(cluster->GetConnectSinkInputNum(), numOperations - i - 1);
@@ -251,4 +270,4 @@ HWTEST_F(HpaeVirtualProcessClusterTest, MultipleConnectDisconnectOperations, Tes
 }
 } // namespace HPAE
 } // namespace AudioStandard
-} // namespace OHOS
+} // namespace OHOS
