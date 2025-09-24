c75207da8f5abf994bffd6b747b2baa39add8fa5
ruiyangsun <sunruiyang5@h-partners.com>
2025-09-24 16:06:40 +0800

    fix build bug
    Signed-off-by: ruiyangsun <sunruiyang5@h-partners.com>

--- services/audio_service/test/unittest/include/audio_unit_test.h
+++ services/audio_service/test/unittest/include/audio_unit_test.h
@@ -1,8 +1,26 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 #ifndef AUDIO_UNIT_TEST_H
 #define AUDIO_UNIT_TEST_H
 
 #include "accesstoken_kit.h"
 #include "token_setproc.h"
+
+#define DEC 10
 
 namespace OHOS::AudioStandard {
 class MockNative {
@@ -29,7 +47,7 @@ public:
             return;
         }
         pos += target.length();
-        MockNative::native = strtoull(dumpInfo.c_str() + pos, nullptr, 10);
+        MockNative::native = strtoull(dumpInfo.c_str() + pos, nullptr, DEC);
     }
 
 private:
