71a767b35f54906607b55ead8941eb3c671b0c3e
timelessxp_gitcode <hexiangpeng1@huawei.com>
2025-09-28 11:25:08 +0800

    update: 更新文件 napi_dfx_utils.h
    Signed-off-by: timelessxp_gitcode <hexiangpeng1@huawei.com>

--- frameworks/js/napi/common/napi_dfx_utils.h
+++ frameworks/js/napi/common/napi_dfx_utils.h
@@ -26,15 +26,15 @@ public:
     static void SendVolumeApiInvokeEvent(int32_t uid, const std::string &functionName, int32_t paramValue);
 
     struct SteamDirection {
-        static constexpr bool PLAYBACK = false;
-        static constexpr bool CAPTURE = true;
+        static constexpr bool playback = false;
+        static constexpr bool capture = true;
     };
  
     struct MainThreadCallFunc {
-        static constexpr uint8_t WRITE = 0;
-        static constexpr uint8_t WRITECB = 1;
-        static constexpr uint8_t READ = 0;
-        static constexpr uint8_t READCB = 1;
+        static constexpr uint8_t write = 0;
+        static constexpr uint8_t writeCb = 1;
+        static constexpr uint8_t read = 0;
+        static constexpr uint8_t readCb = 1;
     };
  
     static void ReportAudioMainThreadEvent(std::string bundleName, bool direction,
