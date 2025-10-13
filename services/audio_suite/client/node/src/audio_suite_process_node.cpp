c491111db7ce3da1519e90489db2daf705335fa8
zhoufugang <zhoufugang1@h-partners.com>
2025-10-13 19:53:51 +0800

    add init to flush
    Signed-off-by: zhoufugang <zhoufugang1@h-partners.com>

--- services/audio_suite/client/node/src/audio_suite_process_node.cpp
+++ services/audio_suite/client/node/src/audio_suite_process_node.cpp
@@ -108,7 +108,10 @@ std::vector<AudioSuitePcmBuffer*>& AudioSuiteProcessNode::ReadProcessNodePreOutp
 int32_t AudioSuiteProcessNode::Flush()
 {
     finishedPrenodeSet.clear();
-    return DeInit();
+    if (DeInit() == SUCCESS && Init() == SUCCESS) {
+        return SUCCESS;
+    }
+    return ERROR;
 }
 
 int32_t AudioSuiteProcessNode::Connect(const std::shared_ptr<AudioNode>& preNode, AudioNodePortType type)
