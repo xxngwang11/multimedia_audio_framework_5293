6d7e3712af8b6d491e65b70374c384fef66ac396
CodeRepository <suwang1@h-partners.com>
2025-10-21 09:49:40 +0800

    update: 更新文件 audio_proresampler.cpp
    Signed-off-by: CodeRepository <suwang1@h-partners.com>

--- services/audio_engine/plugin/resample/proresampler/audio_proresampler.cpp
+++ services/audio_engine/plugin/resample/proresampler/audio_proresampler.cpp
@@ -153,7 +153,7 @@ int32_t ProResampler::Process11025SampleRate(const float *inBuffer, uint32_t inF
     CHECK_AND_RETURN_RET_LOG(ret == RESAMPLER_ERR_SUCCESS, ret, "Process failed with error %{public}s",
         ErrCodeToString(ret).c_str());
     uint32_t fillSize = 0;
-    if (reserveOutFrameLen >= tmpOutFrameLen) {
+    if (reserveOutFrameLen > tmpOutFrameLen) {
         fillSize = reserveOutFrameLen - tmpOutFrameLen;
     }
     ret = memset_s(buf11025_.data(), fillSize * channels_ * sizeof(float), 0, fillSize * channels_ * sizeof(float));
