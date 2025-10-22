5f5480a92f1bd8a0178df24323782642b4d97a78
xiagaoxiang1 <xiagaoxiang1@huawei.com>
2025-10-22 10:02:30 +0800

    update: 更新文件 audio_policy_server.h
    Signed-off-by: xiagaoxiang1 <xiagaoxiang1@huawei.com>

--- services/audio_policy/server/service/service_main/include/audio_policy_server.h
+++ services/audio_policy/server/service/service_main/include/audio_policy_server.h
@@ -402,7 +402,8 @@ public:
 
     int32_t GetAvailableMicrophones(std::vector<sptr<MicrophoneDescriptor>> &retMicList) override;
 
-    int32_t SetDeviceAbsVolumeSupported(const std::string &macAddress, const bool support, const int32_t volume) override;
+    int32_t SetDeviceAbsVolumeSupported(const std::string &macAddress, const bool support,
+        const int32_t volume = 0) override;
 
     int32_t IsAbsVolumeScene(bool &ret) override;
 
