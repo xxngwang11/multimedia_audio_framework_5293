5beb9549d5274e35cfea933e2f01a5a95ff4ab9d
olivine_2025 <lirui167@huawei.com>
2025-11-05 16:22:35 +0800

    fix unlock bug
    Signed-off-by: olivine_2025 <lirui167@huawei.com>

--- frameworks/native/hdiadapter_new/util/id_handler.cpp
+++ frameworks/native/hdiadapter_new/util/id_handler.cpp
@@ -149,10 +149,11 @@ void IdHandler::DecInfoIdUseCount(uint32_t id)
     uint32_t infoId = id & HDI_ID_INFO_MASK;
     std::lock_guard<std::mutex> lock(infoIdMtx_);
     CHECK_AND_RETURN_LOG(infoIdMap_.count(infoId) != 0, "invalid id %{public}u", id);
-    std::lock_guard<std::mutex> useIdLock(infoIdMap_[infoId].useIdMtx_);
+    std::unique_lock<std::mutex> useIdLock(infoIdMap_[infoId].useIdMtx_);
     infoIdMap_[infoId].useIdSet_.erase(id);
     AUDIO_INFO_LOG("infoId: %{public}u, useCount: %{public}zu", infoId, infoIdMap_[infoId].useIdSet_.size());
     CHECK_AND_RETURN(infoIdMap_[infoId].useIdSet_.size() == 0);
+    useIdLock.unlock();
     infoIdMap_.erase(infoId);
     std::lock_guard<std::mutex> freeLock(freeInfoIdMtx_);
     freeInfoIdSet_.emplace(infoId);
