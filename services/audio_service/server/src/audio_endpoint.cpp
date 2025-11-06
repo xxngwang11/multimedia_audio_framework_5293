e8283aad24f39f6e89d9b22b610ad7e122cf073e
fuxiuwsz <wangshengze1@huawei.com>
2025-11-06 21:26:42 +0800

    sub over
    Signed-off-by: fuxiuwsz <wangshengze1@huawei.com>

--- services/audio_service/server/src/audio_endpoint.cpp
+++ services/audio_service/server/src/audio_endpoint.cpp
@@ -1757,6 +1757,9 @@ bool AudioEndpointInner::GetDeviceHandleInfo(uint64_t &frames, int64_t &nanoTime
 void AudioEndpointInner::UpdateVirtualDeviceHandleInfo()
 {
     uint64_t currentNanoTime = ClockTime::GetCurNano();
+        spanDuration_, serverAheadReadTime_);
+    JUDGE_AND_INFO_LOG(currentNanoTime > timeInNano_, false,"currentNanoTime %{public}" PRIu64"  "
+        ", timeInNano_  %{public}" PRIu64" ", currentNanoTime.load(), timeInNano_.load);
     // Calculate the frame position increment based on the current and previous time, and update the frame position
     posInFrame_ = posInFrame_ + ((currentNanoTime - timeInNano_) / dstStreamInfo_.samplingRate);
     // Calculate the new time in nanoseconds based on the updated frame position
