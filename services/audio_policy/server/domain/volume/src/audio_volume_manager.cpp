a19afa959a586f806e8e2f9222d495387efd61ac
chenyaowen6 <chenyaowen6@huawei.com>
2025-11-08 16:35:52 +0800

    修复安规计时重置时弹窗延迟触发问题
    Signed-off-by: chenyaowen6 <chenyaowen6@huawei.com>

--- services/audio_policy/server/domain/volume/src/audio_volume_manager.cpp
+++ services/audio_policy/server/domain/volume/src/audio_volume_manager.cpp
@@ -913,12 +913,14 @@ void AudioVolumeManager::SetRestoreVolumeLevel(DeviceType deviceType, int32_t cu
 void AudioVolumeManager::OnCheckActiveMusicTime(const std::string &reason)
 {
     AUDIO_INFO_LOG("reason:%{public}s", reason.c_str());
-    CheckActiveMusicTime("Offload");
-    if (std::string("Started") != reason) {
-        startSafeTime_ = 0;
-        startSafeTimeBt_ = 0;
-        startSafeTimeSle_ = 0;
-    }
+    std::thread([this, reason]() {
+        this->CheckActiveMusicTime("Offload");
+        if (std::string("Started") != reason) {
+            startSafeTime_ = 0;
+            startSafeTimeBt_ = 0;
+            startSafeTimeSle_ = 0;
+        }
+    }).detach();
 }
 
 int32_t AudioVolumeManager::CheckActiveMusicTime(const std::string &reason)
