54f26a42b6aea3e41a7bab8091cf0cc2b7aa1af1
gcw_IJjWPZkO <wangbanglun@h-partners.com>
2025-11-15 11:01:36 +0800

    超大函数修改
    Signed-off-by: gcw_IJjWPZkO <wangbanglun@h-partners.com>

--- test/demo/audio_suite/entry/src/main/cpp/multiPipelineEdit/MultiPipelineEdit.h
+++ test/demo/audio_suite/entry/src/main/cpp/multiPipelineEdit/MultiPipelineEdit.h
@@ -1,8 +1,7 @@
 /*
  * Copyright (c) 2025 Huawei Device Co., Ltd. 2025-2025. ALL rights reserved.
  */
-#ifndef MULTIPIPELINEEDIT_H
-#define MULTIPIPELINEEDIT_H
+
 #include <cstdint>
 #include <cstdio>
 #include <cstdlib>
@@ -23,43 +22,40 @@
 #include <./utils/Utils.h>
 // 对外napi接口
 napi_value AudioEditNodeInitMultiPipeline(napi_env env, napi_callback_info info);
-napi_value multiAudioInAndOutInit(napi_env env, napi_callback_info info);
-napi_value multiPipelineEnvPrepare(napi_env env, napi_callback_info info);
-napi_value multiSetFormat(napi_env env, napi_callback_info info);
-napi_value multiSetEqualizerMode(napi_env env, napi_callback_info info);
-napi_value multiSetEqualizerFrequencyBandGains(napi_env env, napi_callback_info info);
-napi_value multiStartFieldEffect(napi_env env, napi_callback_info info);
-napi_value multiStartEnvEffect(napi_env env, napi_callback_info info);
-napi_value multiAddAudioSeparation(napi_env env, napi_callback_info info);
-napi_value multiAddNoiseReduction(napi_env env, napi_callback_info info);
-napi_value multiStartVBEffect(napi_env env, napi_callback_info info);
-napi_value multiSaveFileBuffer(napi_env env, napi_callback_info info);
-napi_value multiGetSecondOutputAudio(napi_env env, napi_callback_info info);
-napi_value multiDeleteSong(napi_env env, napi_callback_info info);
-napi_value destroyMultiPipeline(napi_env env, napi_callback_info info);
+napi_value MultiAudioInAndOutInit(napi_env env, napi_callback_info info);
+napi_value MultiPipelineEnvPrepare(napi_env env, napi_callback_info info);
+napi_value MultiSetFormat(napi_env env, napi_callback_info info);
+napi_value MultiSetEqualizerMode(napi_env env, napi_callback_info info);
+napi_value MultiSetEqualizerFrequencyBandGains(napi_env env, napi_callback_info info);
+napi_value MultiStartFieldEffect(napi_env env, napi_callback_info info);
+napi_value MultiStartEnvEffect(napi_env env, napi_callback_info info);
+napi_value MultiAddAudioSeparation(napi_env env, napi_callback_info info);
+napi_value MultiAddNoiseReduction(napi_env env, napi_callback_info info);
+napi_value MultiStartVBEffect(napi_env env, napi_callback_info info);
+napi_value MultiSaveFileBuffer(napi_env env, napi_callback_info info);
+napi_value MultiGetSecondOutputAudio(napi_env env, napi_callback_info info);
+napi_value MultiDeleteSong(napi_env env, napi_callback_info info);
+napi_value DestroyMultiPipeline(napi_env env, napi_callback_info info);
 
 // 实现多管线功能函数
 void MultiStoreTotalBuffToMap(const char *totalBuff, size_t size, const std::string &key);
 OH_AudioSuite_Result MultiPipelineRenderFrame();
 int32_t MultiWriteDataCallBack(OH_AudioNode *audioNode, void *userData, void *audioData, int32_t audioDataSize,
-    bool *finished);
-OH_AudioSuite_Result MultiSetParamsAndWriteData(OH_AudioNodeBuilder *builder,
-    std::string inputId, OH_AudioNode_Type type);
+                          bool *finished);
+OH_AudioSuite_Result MultiSetParamsAndWriteData(OH_AudioNodeBuilder *builder, std::string inputId, OH_AudioNode_Type type);
 void MultiCreateInputNode(napi_env env, const std::string &inputId, napi_value &napiValue,
-    OH_AudioSuite_Result &result);
+                            OH_AudioSuite_Result &result);
 void MultiUpdateInputNode(const std::string &inputId, unsigned int channels, unsigned int sampleRate,
-    unsigned int bitsPerSample, napi_value &napiValue, OH_AudioSuite_Result &result);
-void MultiReadTrackSamples(OH_AVDemuxer *demuxer, uint32_t trackIndex, int bufferSize, std::atomic<bool> &isEnd,
-    std::shared_ptr<PipelineManager> threadPipelineManager);
+                            unsigned int bitsPerSample, napi_value &napiValue, OH_AudioSuite_Result &result);
+void MultiReadTrackSamples(OH_AVDemuxer *demuxer, uint32_t trackIndex, int buffer_size, std::atomic<bool> &isEnd,
+                      std::shared_ptr<PipelineManager> threadPipelineManager);
 bool MultiGetAudioProperties(OH_AVFormat *trackFormat, int32_t &sampleRate, int32_t &channels, int32_t &bitsPerSample);
 void MultiRunAudioThread(OH_AVDemuxer *demuxer, int32_t fileLength);
-void MultiManageExistingOutputNodes(const std::string &inputId,
-    const std::string &mixerId, OH_AudioSuite_Result &result, std::vector<Node> outPutNodes);
+void MultiManageExistingOutputNodes(const std::string &inputId, const std::string &mixerId, OH_AudioSuite_Result &result,
+                               std::vector<Node> outPutNodes);
 void MultiCreateAndConnectOutputNodes(const std::string &inputId, const std::string &outputId,
-    OH_AudioSuite_Result &result);
+                                 OH_AudioSuite_Result &result);
 void MultiManageOutputNodes(napi_env env, const std::string &inputId, const std::string &outputId,
-    const std::string &mixerId, OH_AudioSuite_Result &result);
+                       const std::string &mixerId, OH_AudioSuite_Result &result);
 int MultiAddEffectNodeToNodeManager(std::string &inputNodeId, std::string &effectNodeId);
 Node MultiCreateNodeByType(std::string uuid, OH_AudioNode_Type nodeType);
-
-#endif
