f5b87e5a081fdc6076082d306912fe7a2ac43236
chenyaowen6 <chenyaowen6@huawei.com>
2025-11-15 10:28:02 +0800

    update: 更新文件 audio_core_service.cpp
    Signed-off-by: chenyaowen6 <chenyaowen6@huawei.com>

--- services/audio_policy/server/service/service_main/src/audio_core_service.cpp
+++ services/audio_policy/server/service/service_main/src/audio_core_service.cpp
@@ -1434,7 +1434,7 @@ int32_t AudioCoreService::FetchOutputDeviceAndRoute(std::string caller, const Au
 
     // this will update volume device map
     audioActiveDevice_.UpdateStreamDeviceMap("FetchOutputDeviceAndRoute");
-    // here will update volume�? must after UpdateStreamDeviceMap
+    // here will update volumeï¼Œ must after UpdateStreamDeviceMap
     UpdateActiveDeviceAndVolumeBeforeMoveSession(outputStreamDescs, reason);
 
     int32_t ret = FetchRendererPipesAndExecute(outputStreamDescs, reason);
