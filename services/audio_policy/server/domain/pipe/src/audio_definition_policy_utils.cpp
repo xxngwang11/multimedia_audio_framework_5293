cddfb4781eba55d1d0e8040656a27298b1744946
yangjcc <yangjiangcheng1@huawei.com>
2025-11-28 20:16:44 +0800

    update: 更新文件 audio_definition_policy_utils.cpp
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- services/audio_policy/server/domain/pipe/src/audio_definition_policy_utils.cpp
+++ services/audio_policy/server/domain/pipe/src/audio_definition_policy_utils.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef LOG_TAGxx
+#ifndef LOG_TAG
 #define LOG_TAG "AudioDefinitionPolicyUtils"
 #endif
 
