52c214ccef2a916b27bd495de07d233955ea8eb6
hexuhui <hexuhui3@h-partners.com>
2025-11-29 16:53:32 +0800

    restore
    Signed-off-by: hexuhui <hexuhui3@h-partners.com>

--- frameworks/native/audiorenderer/src/audio_renderer.cpp
+++ frameworks/native/audiorenderer/src/audio_renderer.cpp
@@ -2429,8 +2429,7 @@ bool AudioRendererPrivate::SwitchToTargetStream(IAudioStream::StreamClass target
     // Activate audio interrupt again when restoring for audio server died.
     if (restoreInfo.restoreReason == SERVER_DIED) {
         HandleAudioInterruptWhenServerDied();
-        int32_t ret = InitOutputDeviceChangeCallback();
-        CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, false, "InitOutputDeviceChangeCallback Failed ret:%{public}d", ret);
+        InitOutputDeviceChangeCallback();
     }
     InitAudioRouteCallback();
     isSwitching_ = false;
