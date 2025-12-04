60666122a42560d14b68a036f5b007d2daabfbbb
Charles <wangchao685@h-partners.com>
2025-12-04 16:43:33 +0800

    voulme指令优化
    Signed-off-by: Charles <wangchao685@h-partners.com>

--- services/audio_service/common/src/audio_tool_calculate.cpp
+++ services/audio_service/common/src/audio_tool_calculate.cpp
@@ -18,9 +18,6 @@
 #include "audio_utils.h"
 namespace OHOS {
 namespace AudioStandard {
-#if USE_ARM_NEON == 1
-// constexpr int ALIGIN_FLOAT_SIZE = 8;
-#endif
 
 inline bool Is16ByteAligned(const void * ptr) {
     uintptr_t address = reinterpret_cast<uintptr_t>(ptr);
