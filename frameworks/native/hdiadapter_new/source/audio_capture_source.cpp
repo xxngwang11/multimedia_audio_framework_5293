6466f89529923b1bc4c673a209a6784de6a93608
yangjcc <yangjiangcheng1@huawei.com>
2025-12-04 18:11:29 +0800

    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com> yjctest

--- frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
+++ frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
@@ -769,10 +769,8 @@ enum AudioInputType AudioCaptureSource::ConvertToHDIAudioInputType(int32_t sourc
             break;
         case SOURCE_TYPE_MIC:
         case SOURCE_TYPE_PLAYBACK_CAPTURE:
-            hdiAudioInputType = AUDIO_INPUT_MIC_TYPE;
-            break;
         case SOURCE_TYPE_ULTRASONIC:
-            hdiAudioInputType = AUDIO_INPUT_ULTRASONIC_TYPE;
+            hdiAudioInputType = AUDIO_INPUT_MIC_TYPE;
             break;
         case SOURCE_TYPE_WAKEUP:
             hdiAudioInputType = AUDIO_INPUT_SPEECH_WAKEUP_TYPE;
