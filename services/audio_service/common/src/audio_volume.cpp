3dfcc9561ace71c5fb2c79541aedff3a7a550437
qq_63432433 <pengyixuan3@h-partners.com>
2025-12-05 10:01:50 +0800

    update: 更新文件 audio_volume.cpp
    Signed-off-by: qq_63432433 <pengyixuan3@h-partners.com>

--- services/audio_service/common/src/audio_volume.cpp
+++ services/audio_service/common/src/audio_volume.cpp
@@ -353,8 +353,7 @@ inline float AudioVolume::GetAppVolumeInternal(int32_t appUid, AudioVolumeMode m
     float appVolume = 1.0f;
     auto iter = appVolume_.find(appUid);
     if (iter != appVolume_.end()) {
-        appVolume = iter->second.isMuted_ ? iter->second.totalVolume_ :
-            (mode == AUDIOSTREAM_VOLUMEMODE_SYSTEM_GLOBAL ? 1.0 : iter->second.totalVolume_);
+        appVolume = iter->second.totalVolume_;
     }
     return appVolume;
 }
@@ -380,8 +379,7 @@ void AudioVolume::SetAppVolumeMute(int32_t appUid, bool isMuted)
     for (auto &streamVolume : streamVolume_) {
         auto &stream = streamVolume.second;
         if (stream.GetAppUid() == appUid) {
-            stream.appVolume_ = isMuted ? totalAppVolume :
-                (stream.GetVolumeMode() == AUDIOSTREAM_VOLUMEMODE_SYSTEM_GLOBAL ? 1.0 : totalAppVolume);
+            stream.appVolume_ = totalAppVolume;
             stream.totalVolume_ = stream.isMuted_ ? 0.0f :
                 stream.volume_ * stream.duckFactor_ * stream.lowPowerFactor_ * stream.appVolume_;
         }
@@ -441,8 +439,7 @@ void AudioVolume::SetAppVolume(AppVolume &appVolume)
     for (auto &streamVolume : streamVolume_) {
         auto &stream = streamVolume.second;
         if (stream.GetAppUid() == appUid) {
-            stream.appVolume_ = appVolume.isMuted_ ? appVolume.totalVolume_ :
-                (stream.GetVolumeMode() == AUDIOSTREAM_VOLUMEMODE_SYSTEM_GLOBAL ? 1.0 : appVolume.totalVolume_);
+            stream.appVolume_ = appVolume.totalVolume_;
             stream.totalVolume_ = stream.isMuted_ ? 0.0f :
                 stream.volume_ * stream.duckFactor_ * stream.lowPowerFactor_ * stream.appVolume_;
         }
