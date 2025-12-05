e70a5aad6773ca9c02c8215920325d7d25786a75
qyhyue123 <qiuyuhan1@h-partners.com>
2025-12-05 10:07:04 +0800

    fix
    Signed-off-by: qyhyue123 <qiuyuhan1@h-partners.com>

--- services/audio_policy/server/domain/device/src/va/va_device_manager.cpp
+++ services/audio_policy/server/domain/device/src/va/va_device_manager.cpp
@@ -89,19 +89,18 @@ void VADeviceManager::OnDevicesConnected(
     const std::shared_ptr<VADevice> &vaDevice,
     const sptr<IVADeviceController> &controller)
 {
-    {
-        std::lock_guard<std::mutex> lock(statusMutex_);
-        CHECK_AND_RETURN_LOG(vaDevice != nullptr && controller != nullptr, "invalid parameter: null pointer detected");
-        AUDIO_INFO_LOG("va device manager connecting to device: {\"name\":\"%{public}s\", \"type\":\"%{public}d\"}",
-            vaDevice->configuration_.name_.c_str(), vaDevice->configuration_.type_);
-        std::shared_ptr<AudioDeviceDescriptor> descriptor = ConvertVADeviceToDescriptor(vaDevice);
-        connectedVADeviceMap_[vaDevice->configuration_.address_] = controller;
+    CHECK_AND_RETURN_LOG(vaDevice != nullptr && controller != nullptr, "invalid parameter: null pointer detected");
+    statusMutex_.lock();
+    AUDIO_INFO_LOG("va device manager connecting to device: {\"name\":\"%{public}s\", \"type\":\"%{public}d\"}",
+        vaDevice->configuration_.name_.c_str(), vaDevice->configuration_.type_);
+    std::shared_ptr<AudioDeviceDescriptor> descriptor = ConvertVADeviceToDescriptor(vaDevice);
+    connectedVADeviceMap_[vaDevice->configuration_.address_] = controller;
 
-        if (!config_.GetAdapterInfo(AudioAdapterType::TYPE_VA)) {
-            RegisterVAAdapterToMap();
-        }
-        AddVAStreamPropToMap(vaDevice->configuration_.properties_);
+    if (!config_.GetAdapterInfo(AudioAdapterType::TYPE_VA)) {
+        RegisterVAAdapterToMap();
     }
+    AddVAStreamPropToMap(vaDevice->configuration_.properties_);
+    statusMutex_.unlock();
     AudioCoreService::GetCoreService()->GetEventEntry()->OnDeviceStatusUpdated(*descriptor, true);
 }
 
