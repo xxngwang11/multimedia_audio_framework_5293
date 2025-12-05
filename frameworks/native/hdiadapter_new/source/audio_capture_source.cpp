1da2ae3008f50b503b0fa2090b3c9bae75313933
yangjcc <yangjiangcheng1@huawei.com>
2025-12-05 15:08:57 +0800

    update: 更新文件 audio_capture_source.cpp
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
+++ frameworks/native/hdiadapter_new/source/audio_capture_source.cpp
@@ -769,8 +769,8 @@ enum AudioInputType AudioCaptureSource::ConvertToHDIAudioInputType(int32_t sourc
             break;
         case SOURCE_TYPE_MIC:
         case SOURCE_TYPE_PLAYBACK_CAPTURE:
-        case SOURCE_TYPE_ULTRASONIC:
-            hdiAudioInputType = AUDIO_INPUT_MIC_TYPE;
+        case SOURCE_TYPE_ULTRASONIC: //The configuration of the platform before CHS.
+            hdiAudioInputType = AUDIO_INPUT_MIC_TYPE; 
             break;
         case SOURCE_TYPE_WAKEUP:
             hdiAudioInputType = AUDIO_INPUT_SPEECH_WAKEUP_TYPE;
