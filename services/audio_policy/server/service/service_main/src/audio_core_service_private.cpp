9d997748caec621737454fdb569b512a08f55836
RogerHere <luohengou@huawei.com>
2025-12-06 17:40:05 +0800

    update: 更新文件 audio_core_service_private.cpp
    Signed-off-by: RogerHere <luohengou@huawei.com>

--- services/audio_policy/server/service/service_main/src/audio_core_service_private.cpp
+++ services/audio_policy/server/service/service_main/src/audio_core_service_private.cpp
@@ -2627,7 +2627,7 @@ void AudioCoreService::PrepareMoveAttrs(std::shared_ptr<AudioStreamDescriptor> &
     oldSinkName = AudioPolicyUtils::GetInstance().GetSinkName(streamDesc->oldDeviceDescs_.front(),
         streamDesc->sessionId_);
 
-    HILOG_COMM_INFO("[StreamExecInfo] Move stream %{public}u [%{public}d][%{public}s] to [%{public}d][%{public}s]" \
+    AUDIO_INFO_LOG("[StreamExecInfo] Move stream %{public}u [%{public}d][%{public}s] to [%{public}d][%{public}s]" \
         " reason %{public}d",
         streamDesc->sessionId_, streamDesc->oldDeviceDescs_.front()->deviceType_,
         GetEncryptAddr(streamDesc->oldDeviceDescs_.front()->macAddress_).c_str(), newDeviceDesc->deviceType_,
