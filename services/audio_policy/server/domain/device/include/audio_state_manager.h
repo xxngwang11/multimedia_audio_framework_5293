1d4b4638ab08455dda20468257782c00cce37670
huyanbo6 <huyanbo6@h-partners.com>
2025-12-08 14:42:56 +0800

    Signed-off-by: huyanbo6 <huyanbo6@h-partners.com>
    change

--- services/audio_policy/server/domain/device/include/audio_state_manager.h
+++ services/audio_policy/server/domain/device/include/audio_state_manager.h
@@ -90,7 +90,7 @@ public:
         const shared_ptr<AudioDeviceDescriptor> &audioDeviceDescriptor);
 
     void SetAudioSceneOwnerUid(const int32_t uid);
-    
+    int32_t GetAudioSceneOwnerUid();
     int32_t SetAudioClientInfoMgrCallback(sptr<IStandardAudioPolicyManagerListener> &callback);
     
     int32_t SetAudioVKBInfoMgrCallback(sptr<IStandardAudioPolicyManagerListener> &callback);
