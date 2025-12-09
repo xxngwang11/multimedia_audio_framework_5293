1b48b82388c6cb68b9791fc37d0aa3150acf39c8
gcw_vYiIpahY <zhaiyongqi1@h-partners.com>
2025-12-09 15:34:50 +0800

    CodeCheck多余空格
    Signed-off-by: gcw_vYiIpahY <zhaiyongqi1@h-partners.com>

--- test/demo/audio_suite/entry/src/main/cpp/audioEffectNode/Input.cpp
+++ test/demo/audio_suite/entry/src/main/cpp/audioEffectNode/Input.cpp
@@ -270,7 +270,7 @@ int32_t WriteDataCallBack(OH_AudioNode *audioNode, void *userData, void *audioDa
     if (!CheckParameters(audioNode, audioData, finished)) {
         return 0;
     }
-    // Processing audio is nullptr here, it is an issue with the demo's method of obtaining audio data, 
+    // Processing audio is nullptr here, it is an issue with the demo's method of obtaining audio data,
     // not a problem with the underlying interface
     std::string inputId = static_cast<UserData *>(userData)->id;
     auto usetDataIt = g_userDataMap.find(inputId);
