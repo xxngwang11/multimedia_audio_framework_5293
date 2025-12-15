08c8061ab5456cae63cb999bfae33cf039524b19
zhongshaopeng <zhongshaopeng@h-partners.com>
2025-12-15 14:03:51 +0800

    move lock
    Signed-off-by: zhongshaopeng <zhongshaopeng@h-partners.com>

--- frameworks/native/hdiadapter_new/adapter/bluetooth_device_manager.cpp
+++ frameworks/native/hdiadapter_new/adapter/bluetooth_device_manager.cpp
@@ -57,6 +57,7 @@ int32_t BluetoothDeviceManager::LoadAdapter(const std::string &adapterName)
     CHECK_AND_RETURN_RET(index >= 0, ERR_NOT_STARTED);
 
     struct AudioAdapter *adapter = nullptr;
+    std::lock_guard<std::mutex> lock(adapterMtx_);
     ret = audioManager_->LoadAdapter(audioManager_, &(descs[index]), &adapter);
     if (ret != SUCCESS) {
         HdiMonitor::ReportHdiException(HdiType::A2DP, ErrorCase::CALL_HDI_FAILED, ret, (adapterName +
@@ -65,7 +66,6 @@ int32_t BluetoothDeviceManager::LoadAdapter(const std::string &adapterName)
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS && adapter != nullptr, ERR_NOT_STARTED, "load adapter fail");
     ret = adapter->InitAllPorts(adapter);
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ERR_NOT_STARTED, "init all ports fail");
-    std::lock_guard<std::mutex> lock(adapterMtx_);
     adapters_[adapterName] = std::make_shared<BluetoothAdapterWrapper>();
     adapters_[adapterName]->adapterDesc_ = descs[index];
     adapters_[adapterName]->adapter_ = adapter;
