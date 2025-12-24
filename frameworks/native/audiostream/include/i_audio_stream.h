9e24bdd19d4d533ae6edf0c33933cc2f2fb7953e
yangjcc <yangjiangcheng1@huawei.com>
2025-12-24 16:42:09 +0800

    update: 更新文件 i_audio_stream.h
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- frameworks/native/audiostream/include/i_audio_stream.h
+++ frameworks/native/audiostream/include/i_audio_stream.h
@@ -10,7 +10,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.sss
+ * limitations under the License.
  */
 #ifndef I_AUDIO_STREAM_H
 #define I_AUDIO_STREAM_H
