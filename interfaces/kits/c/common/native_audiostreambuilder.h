c9e0e36f0369a5ec6f114a0d77a4e9c0fe52d83f
yangjcc <yangjiangcheng1@huawei.com>
2025-12-24 21:07:53 +0800

    update: 更新文件 native_audiostreambuilder.h
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- interfaces/kits/c/common/native_audiostreambuilder.h
+++ interfaces/kits/c/common/native_audiostreambuilder.h
@@ -11,7 +11,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ss
+ */
 
 /**
  * @addtogroup OHAudio
@@ -474,6 +474,27 @@ OH_AudioStream_Result OH_AudioStreamBuilder_SetCapturerFastStatusChangeCallback(
  * @since 23
 */
 OH_AudioStream_Result OH_AudioStreamBuilder_SetRendererKeepRunning(OH_AudioStreamBuilder* builder, bool keepRunning);
+
+/**
+ * Sets target mode when using playback capture. Mode will decide what kind of streams to capture.
+ * This function is only available for {@link #AUDIOSTREAM_TYPE_CAPTURER} type.
+ * After setting playback capture mode, the {@link #OH_AudioStream_SourceType} will be ignored, so
+ * caller do not need to use {@link #OH_AudioStreamBuilder_SetCapturerInfo} if you only want to capture
+ * playback streams.
+ * Note that playback capture is only available for specific system applications currently, others do
+ * not have authorization.
+ *
+ * @param builder Reference provided by OH_AudioStreamBuilder_Create().
+ * @param mode The playback capture mode to set. This can be a combination of the available
+ *     {@link #uint32_t}.
+ * @return Function result code:
+ *     {@link #AUDIOSTREAM_SUCCESS} If the execution is successful.
+ *     {@link #AUDIOSTREAM_ERROR_INVALID_PARAM} 1.The param of builder is nullptr;
+ *                                             2.The param of mode is invalid.
+ * @since 23
+ */
+OH_AudioStream_Result OH_AudioStreamBuilder_SetPlaybackCaptureMode(OH_AudioStreamBuilder* builder,
+    uint32_t mode);
 
 #ifdef __cplusplus
 }
