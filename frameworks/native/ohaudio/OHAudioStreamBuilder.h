912e4ecf946c5286a90736bbaea417f9d42cd786
yangjcc <yangjiangcheng1@huawei.com>
2025-12-24 21:06:14 +0800

    update: 更新文件 OHAudioStreamBuilder.h
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- frameworks/native/ohaudio/OHAudioStreamBuilder.h
+++ frameworks/native/ohaudio/OHAudioStreamBuilder.h
@@ -10,8 +10,8 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.ss
- */ss
+ * limitations under the License.
+ */
 
 #ifndef OH_AUDIO_STREAM_BUILDER_H
 #define OH_AUDIO_STREAM_BUILDER_H
