97b07bc10e9825c81ed451659debb739627bac4f
Bigdemon <lianghanlin@h-partners.com>
2025-12-24 11:17:14 +0800

    update: 更新文件 audio_suite_pure_voice_change_node_test.cpp
    Signed-off-by: Bigdemon <lianghanlin@h-partners.com>

--- services/audio_suite/test/unittest/node/audio_suite_pure_voice_change_node_test.cpp
+++ services/audio_suite/test/unittest/node/audio_suite_pure_voice_change_node_test.cpp
@@ -134,7 +134,7 @@ int32_t AudioSuitePureVoiceChangeNodeTest::DoprocessTest(AudioPureVoiceChangeTyp
     std::shared_ptr<AudioSuitePureVoiceChangeNode> node = std::make_shared<AudioSuitePureVoiceChangeNode>();
     node->Init();
     std::shared_ptr<MockInputNode> mockInputNode_ = std::make_shared<MockInputNode>();
-    OutputPort<AudioSuitePcmBuffer*>* inputNodeOutputPort =
+    std::shared_ptr<OutputPort<AudioSuitePcmBuffer*>> inputNodeOutputPort =
         std::make_shared<OutputPort<AudioSuitePcmBuffer*>>(mockInputNode_);
     EXPECT_CALL(*mockInputNode_, GetOutputPort())
         .Times(g_expectedGetOutputPortCalls).WillRepeatedly(::testing::Return(inputNodeOutputPort.get()));
@@ -349,10 +349,10 @@ HWTEST_F(AudioSuitePureVoiceChangeNodeTest, DoProcessTest_002, TestSize.Level0)
     node->Init();
     node->SetBypassEffectNode(true);
     std::shared_ptr<MockInputNode> mockInputNode_ = std::make_shared<MockInputNode>();
-    OutputPort<AudioSuitePcmBuffer*>* inputNodeOutputPort =
+    std::shared_ptr<OutputPort<AudioSuitePcmBuffer*>> inputNodeOutputPort =
         std::make_shared<OutputPort<AudioSuitePcmBuffer*>>(mockInputNode_);
     EXPECT_CALL(*mockInputNode_, GetOutputPort())
-        .Times(g_expectedGetOutputPortCalls).WillRepeatedly(::testing::Return(inputNodeOutputPort));
+        .Times(g_expectedGetOutputPortCalls).WillRepeatedly(::testing::Return(inputNodeOutputPort.get()));
 
     node->Connect(mockInputNode_);
     EXPECT_EQ(1, inputNodeOutputPort->GetInputNum());
