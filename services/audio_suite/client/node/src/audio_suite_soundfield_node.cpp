248f7bd66422312c97f3c02bb0bc0b38f35dc0bb
Bigdemon <lianghanlin@h-partners.com>
2025-12-29 18:23:59 +0800

    update: 更新文件 audio_suite_soundfield_node.cpp
    Signed-off-by: Bigdemon <lianghanlin@h-partners.com>

--- services/audio_suite/client/node/src/audio_suite_soundfield_node.cpp
+++ services/audio_suite/client/node/src/audio_suite_soundfield_node.cpp
@@ -24,9 +24,6 @@ namespace OHOS {
 namespace AudioStandard {
 namespace AudioSuite {
 namespace {
-static constexpr AudioSamplingRate SOUNDFIELD_ALGO_SAMPLE_RATE = SAMPLE_RATE_48000;
-static constexpr AudioSampleFormat SOUNDFIELD_ALGO_SAMPLE_FORMAT = SAMPLE_S16LE;
-static constexpr AudioChannel SOUNDFIELD_ALGO_CHANNEL_COUNT = STEREO;
 static constexpr AudioChannelLayout SOUNDFIELD_ALGO_CHANNEL_LAYOUT = CH_LAYOUT_STEREO;
 
 static const std::unordered_map<SoundFieldType, iMedia_Surround_PARA> soundFieldParaMap = {
@@ -38,14 +35,7 @@ static const std::unordered_map<SoundFieldType, iMedia_Surround_PARA> soundField
 }
 
 AudioSuiteSoundFieldNode::AudioSuiteSoundFieldNode()
-    : AudioSuiteProcessNode(AudioNodeType::NODE_TYPE_SOUND_FIELD,
-          AudioFormat{{SOUNDFIELD_ALGO_CHANNEL_LAYOUT, SOUNDFIELD_ALGO_CHANNEL_COUNT},
-              SOUNDFIELD_ALGO_SAMPLE_FORMAT,
-              SOUNDFIELD_ALGO_SAMPLE_RATE}),
-      outPcmBuffer_(PcmBufferFormat{SOUNDFIELD_ALGO_SAMPLE_RATE,
-          SOUNDFIELD_ALGO_CHANNEL_COUNT,
-          SOUNDFIELD_ALGO_CHANNEL_LAYOUT,
-          SOUNDFIELD_ALGO_SAMPLE_FORMAT})
+    : AudioSuiteProcessNode(AudioNodeType::NODE_TYPE_SOUND_FIELD)
 {}
 
 AudioSuiteSoundFieldNode::~AudioSuiteSoundFieldNode()
@@ -64,6 +54,17 @@ int32_t AudioSuiteSoundFieldNode::Init()
 
     int32_t ret = algoInterface_->Init();
     CHECK_AND_RETURN_RET_LOG(ret == SUCCESS, ret, "Failed to Init soundField algorithm");
+
+    InitAudioFormat(AudioFormat{{SOUNDFIELD_ALGO_CHANNEL_LAYOUT, nodeCapability.inChannels},
+        static_cast<AudioSampleFormat>(nodeCapability.inFormat),
+        static_cast<AudioSamplingRate>(nodeCapability.inSampleRate)});
+
+    outPcmBuffer_ = AudioSuitePcmBuffer(PcmBufferFormat{static_cast<AudioSamplingRate>(nodeCapability.outSampleRate),
+        nodeCapability.outChannels,
+        SOUNDFIELD_ALGO_CHANNEL_LAYOUT,
+        static_cast<AudioSampleFormat>(nodeCapability.outFormat)});
+    CHECK_AND_RETURN_RET_LOG(nodeCapability.inSampleRate != 0, ERROR, "Invalid input SampleRate");
+    pcmDurationMs_ = nodeCapability.frameLen / nodeCapability.inSampleRate * MILLISECONDS_TO_MICROSECONDS;
 
     AUDIO_INFO_LOG("AudioSuiteSoundFieldNode::Init end");
     return SUCCESS;
