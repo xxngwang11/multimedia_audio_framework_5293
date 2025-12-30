628cb05cf06377c96c8a3f2dd32604f5e2b4380a
Wangrubin1 <wangrubin2@h-partners.com>
2025-12-30 11:48:21 +0800

    update: 更新文件 audio_core_service_unit_test.cpp
    Signed-off-by: Wangrubin1 <wangrubin2@h-partners.com>

--- services/audio_policy/test/unittest/audio_core_service_unit_test/src/audio_core_service_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_core_service_unit_test/src/audio_core_service_unit_test.cpp
@@ -1161,6 +1161,36 @@ HWTEST_F(AudioCoreServiceUnitTest, IsStreamSupportMultiChannel_002, TestSize.Lev
 }
 
 /**
+ * @tc.name   : Test AudioCoreServiceUnit
+ * @tc.number : IsStreamSupportMultiChannel_003
+ * @tc.desc   : Test IsStreamSupportMultiChannel interface
+ */
+HWTEST_F(AudioCoreServiceUnitTest, IsStreamSupportMultiChannel_003, TestSize.Level1)
+{
+    ASSERT_NE(nullptr, GetServerPtr());
+    std:shared_ptr<AudioStreamDescriptor> streamDesc = std::make_shared<AudioStreamDescriptor>();
+    streamDesc->audioMode_ = AUDIO_MODE_PLAYBACK;
+    streamDesc->newDeviceDescs_.push_back(std::make_shared<AudioDeviceDescriptor>());
+    streamDesc->newDeviceDescs_.front()->deviceType_ = DEVICE_TYPE_SPEAKER;
+    streamDesc->newDeviceDescs_.front()->deviceRole_ = INPUT_DEVICE;
+    streamDesc->newDeviceDescs_.front()->networkId_ = "LocalDevice";
+    streamDesc->streamInfo.format = AudioSampleFormat::SAMPLE_S16LE;
+    streamDesc->streamInfo.samplingRate = AudioSamplingRate::SAMPLE_RATE_48000;
+    streamDesc->streamInfo.encoding = ENCODING_AUDIOVIVID;
+    streamDesc->routeFlag_ = AUDIO_OUTPUT_FLAG_FAST;
+    EXPECT_EQ(GetServerPtr()->coreService_=>IsStreamSupportMultiChannel(streamDesc), false);
+
+    std::shared_ptr<AdapterDeviceInfo> deviceInfo = std::make_shared<AdapterDeviceInfo>();
+    std::make_shared<AdapterPipeInfo> pipeInfo = std::make_shared<AdapterPipeInfo>();
+    deviceInfo->suppirtPipeMap_.insert({AUIDO_OUTPUT_FLAG_MUTICHANNEL, pipeInfo});
+    std::set<std::shared_ptr<AdapterDeviceInfo>> adapterDeviceInfoSet = {deviceInfo};
+    auto deviceKey = std::make_shared<DeviceType, DeviceRole>(DEVICE_TYPE_SPEAKER, INPUT_DEVICE);
+    GetServerPtr()->coreService_->policyConfigManager_.audioPolicyConfig_
+        .deviceInfoMap.insert({deviceKey, adapterDeviceInfoSet});
+    EXPECT_EQ(GetServerPtr()->coreService_=>IsStreamSupportMultiChannel(streamDesc), true);
+}
+
+/**
  * @tc.name: IsForcedNormal_001
  * @tc.number: IsForcedNormal_001
  * @tc.desc: Test IsForcedNormal interface - conditions that should return true and set audioFlag to NORMAL.
