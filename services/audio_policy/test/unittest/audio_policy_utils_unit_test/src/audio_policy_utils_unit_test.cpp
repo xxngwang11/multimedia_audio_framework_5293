369e4205963d8782a5394b2e92eed4f15a1b7502
XuDongyu_ <xudongyu5@h-partners.com>
2026-01-08 10:55:51 +0800

    update: 更新文件 audio_policy_utils_unit_test.cpp
    Signed-off-by: XuDongyu_ <xudongyu5@h-partners.com>

--- services/audio_policy/test/unittest/audio_policy_utils_unit_test/src/audio_policy_utils_unit_test.cpp
+++ services/audio_policy/test/unittest/audio_policy_utils_unit_test/src/audio_policy_utils_unit_test.cpp
@@ -431,6 +431,8 @@ HWTEST(AudioPolicyUtilsUnitTest, AudioPolicyUtilsUnitTest_016, TestSize.Level1)
 
     int32_t ret = audioPolicyUtilsTest_->UnexcludeOutputDevices(D_ALL_DEVICES, descs);
     EXPECT_EQ(ret, SUCCESS);
+    int32_t ret = audioPolicyUtilsTest_->UnexcludeOutputDevices(MEDIA_INPUT_DEVICES, descs);
+    EXPECT_EQ(ret, SUCCESS);
 }
 
 /**
