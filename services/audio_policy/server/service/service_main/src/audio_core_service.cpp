72c852366633dcf7abbc20d97214ed7affbe5937
sunruiyang <sunruiyang5@h-partners.com>
2026-01-23 14:39:26 +0800

    fix bug
    Signed-off-by: sunruiyang <sunruiyang5@h-partners.com>

--- services/audio_policy/server/service/service_main/src/audio_core_service.cpp
+++ services/audio_policy/server/service/service_main/src/audio_core_service.cpp
@@ -1,4 +1,4 @@
-c/*
+/*
  * Copyright (c) 2025-2026 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
