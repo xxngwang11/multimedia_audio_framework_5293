22e76b8308a349c1789343dc7d1825aa6a5ce208
huyanbo6 <huyanbo6@h-partners.com>
2026-01-24 18:02:42 +0800

    update: 更新文件 audio_core_service_private.cpp
    Signed-off-by: huyanbo6 <huyanbo6@h-partners.com>

--- services/audio_policy/server/service/service_main/src/audio_core_service_private.cpp
+++ services/audio_policy/server/service/service_main/src/audio_core_service_private.cpp
@@ -346,6 +346,7 @@ void AudioCoreService::BluetoothScoFetch(std::shared_ptr<AudioStreamDescriptor> 
     if (streamDesc->streamStatus_ != STREAM_STATUS_STARTED) {
         return;
     }
+    CHECK_AND_CONTINUE_LOG(pipeManager_ != nullptr, "pipeManager_ is nullptr");
     bool hasRunningRecognitionCapturerStream = pipeManager_->HasRunningRecognitionCapturerStream();
     if (desc->isVrSupported_ &&
         (Util::IsScoSupportSource(streamDesc->capturerInfo_.sourceType) || hasRunningRecognitionCapturerStream)) {
