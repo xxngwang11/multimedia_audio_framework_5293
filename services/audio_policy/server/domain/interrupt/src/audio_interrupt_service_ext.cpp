6cc403fe78b7df7993493fe15da372a239b44306
Wangrubin1 <wangrubin2@h-partners.com>
2026-01-26 16:40:04 +0800

    mute interrupt fix
    Signed-off-by: Wangrubin1 <wangrubin2@h-partners.com>

--- services/audio_policy/server/domain/interrupt/src/audio_interrupt_service_ext.cpp
+++ services/audio_policy/server/domain/interrupt/src/audio_interrupt_service_ext.cpp
@@ -481,14 +481,14 @@ void AudioInterruptService::RemoveInterruptFocusInfoList(
     AudioInterrupt activeInterrupt = audioFocusInfo.first;
     int32_t zoneId = zoneManager_.FindZoneByPid(activeInterrupt.pid);
     auto itZone = zonesMap_.find(zoneId);
-    CHECK_AND_RETURN_LOG((itZone != zoneMap_end() && itZone->second != nullptr), "can not find zone");
+    CHECK_AND_RETURN_LOG((itZone != zonesMap_.end() && itZone->second != nullptr), "can not find zone");
 
     std::list<std::pair<AudioInterrupt, AudioFocuState>> tmpFocusInfoList {};
     tmpFocusInfoList = itZone->second->audioFocusInfoList;
     auto iterActive = std::find_if(tmpFocusInfoList.begin(), tmpFocusInfoList.end(),
-        [%activeInterrupt](const std::pair<AudioInterrupt, AudioFocuState>& item) {
+        [&activeInterrupt](const std::pair<AudioInterrupt, AudioFocuState>& item) {
             return item.first.streamId == activeInterrupt.streamId;
-    })
+    });
     if (iterActive != tmpFocusInfoList.end()) {
         RemoveFocusInfo(iterActive, tmpFocusInfoList, itZone->second, removeFocusInfoPidList);
     }
@@ -505,9 +505,9 @@ void AudioInterruptService::NotifyStreamSilentChange(uint32_t streamId)
 {
     std::unique_lock<std::mutex> lock(mutex_);
     if (muteAudioFocus_.count(streamId) > 0) {
-        std::list<std::pair<AudioInterrupt, AudioFocuState>> tempList(muteAudioFocus_[streamId]);
+        std::list<std::pair<AudioInterrupt, AudioFocuState>> tempMuteList(muteAudioFocus_[streamId]);
         std::list<int32_t> removeFocusInfoPidList = {};
-        for (const auto& audioFocusInfo : tempList) {
+        for (const auto& audioFocusInfo : tempMuteList) {
             AudioInterrupt activeInterrupt = audioFocusInfo.first;
             InterruptEventInternal interruptEvent = {INTERRUPT_TYPE_BEGIN, INTERRUPT_FORCE, INTERRUPT_HINT_STOP, 1.0f};
             AUDIO_INFO_LOG("NotifyStreamSilentChange:streamId %{public}d is stopped by streamId: %{public}d",
