1e79b7f88be7b33b127b5e53a2cdeb81184a2678
yangjcc <yangjiangcheng1@huawei.com>
2026-01-27 14:55:46 +0800

    update: 更新文件 capturer_in_server_fuzzer.cpp
    Signed-off-by: yangjcc <yangjiangcheng1@huawei.com>

--- test/fuzztest/capturerinserver_fuzzer/capturer_in_server_fuzzer.cpp
+++ test/fuzztest/capturerinserver_fuzzer/capturer_in_server_fuzzer.cpp
@@ -121,11 +121,7 @@ public:
         return bufferDesc;
     }
 };
-
 int32_t EnqueueBuffer(const BufferDesc &bufferDesc) override { return 0; }
-void AbortCallback(int32_t abortTimes) override { return; }
-};
-
 class ConcreteIStreamListener : public IStreamListener {
     int32_t OnOperationHandled(Operation operation, int64_t result) { return SUCCESS; }
 };
